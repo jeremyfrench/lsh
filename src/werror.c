14b3895a849f1972ce39fb7e456ecc3751302039
Niels Möller <nisse@lysator.liu.se>
1998-08-31 14:27:09 +0200

    New functions debug(), debug_safe() and werror_safe(). New global variables quiet_flag and debug_flag.
    Rev: src/werror.c:1.2

--- src/werror.c
+++ src/werror.c
@@ -7,13 +7,73 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int debug_flag = 0;
+int quiet_flag = 0;
+
 void werror(char *format, ...) 
 {
   va_list args;
 
-  va_start(args, format);
-  vfprintf(stderr, format, args);
-  va_end(args);
+  if (!quiet_flag)
+    {
+      va_start(args, format);
+      vfprintf(stderr, format, args);
+      va_end(args);
+    }
+}
+
+void debug(char *format, ...) 
+{
+  va_list args;
+
+  if (debug_flag)
+    {
+      va_start(args, format);
+      vfprintf(stderr, format, args);
+      va_end(args);
+    }
+}
+
+/* Escape non-printable characters. */
+void werror_washed(UINT32 length, UINT8 *msg)
+{
+  int i;
+
+  for(i = 0; i<lengh; i++)
+    {
+      switch(msg[i])
+	{
+	case '\\':
+	  fputs("\\\\", stderr);
+	  break;
+	case '\r':
+	  /* Ignore */
+	  break;
+	default:
+	  if (!isprint(msg[i]))
+	    {
+	      fprintf("\\x%2x", msg[i]);
+	      break;
+	    }
+	  /* Fall through */
+	case '\n':
+	  putc('\n', stderr);
+	  break;
+	}
+    }
+}
+
+/* For outputting data recieved from the other end */
+void werror_safe(UINT32 length, UINT8 *msg)
+{
+  if (!quiet_flag)
+    werror_washed(length, msg);
+}
+
+void debug_safe(UINT32 length, UINT8 *msg)
+{
+  if (debug_flag)
+    werror_washed(length, msg);
 }
 
 void fatal(char *format, ...) 
