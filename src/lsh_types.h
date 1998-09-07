648d39ffb2ac5cded7f18fe332250c66a521aa6f
Niels Möller <nisse@lysator.liu.se>
1998-09-07 19:00:05 +0200

    Fixed WRITE_UINT32 and READ_UINT32 macros.
    Rev: src/lsh_types.h:1.7

--- src/lsh_types.h
+++ src/lsh_types.h
@@ -43,16 +43,16 @@
 /* Reads a 32-bit integer, in network byte order */
 #define READ_UINT32(p)				\
 ((((UINT32) (p)[0]) << 24)			\
- | (((UINT32) (p)[0]) << 16)			\
- | (((UINT32) (p)[0]) << 8)			\
- | ((UINT32) (p)[0]))
+ | (((UINT32) (p)[1]) << 16)			\
+ | (((UINT32) (p)[2]) << 8)			\
+ | ((UINT32) (p)[3]))
 
 #define WRITE_UINT32(p, i)			\
 do {						\
   (p)[0] = ((i) >> 24) & 0xff;			\
-  (p)[0] = ((i) >> 16) & 0xff;			\
-  (p)[0] = ((i) >> 8) & 0xff;			\
-  (p)[0] = (i) & 0xff;				\
+  (p)[1] = ((i) >> 16) & 0xff;			\
+  (p)[2] = ((i) >> 8) & 0xff;			\
+  (p)[3] = (i) & 0xff;				\
 } while(0)
 
 #define MIN(a, b) (((a)>(b)) ? (b) : (a))
