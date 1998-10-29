ad960c01bfa72cbcdee0a7ed9a1937fd78524a81
Niels Möller <nisse@lysator.liu.se>
1998-10-29 17:24:12 +0100

    *** empty log message ***
    Rev: src/io.h:1.14

--- src/io.h
+++ src/io.h
@@ -118,6 +118,8 @@ struct io_backend
   struct callout *callouts;
 };
 
+void init_backend(struct io_backend *b);
+
 void io_run(struct io_backend *b);
 
 int get_inaddr(struct sockaddr_in	* addr,
@@ -148,10 +150,10 @@ struct io_fd *io_read(struct io_backend *b,
 		      struct read_handler *read_callback,
 		      struct close_callback *close_callback);
 
-struct io_fd *io_write(struct io_backend *b,
-		       int fd,
-		       UINT32 block_size,
-		       struct close_callback *close_callback);
+struct abstract_write *io_write(struct io_backend *b,
+				int fd,
+				UINT32 block_size,
+				struct close_callback *close_callback);
 
 
 #endif /* LSH_IO_H_INCLUDED */
