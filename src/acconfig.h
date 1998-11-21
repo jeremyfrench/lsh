ee99e0ac7838926372e0f2fec7d98287d4fe0bfe
Niels Möller <nisse@lysator.liu.se>
1998-11-22 00:53:21 +0100

    Use automake.
    Rev: src/Makefile.am:1.1
    Rev: src/Makefile.in:1.27(DEAD)
    Rev: src/acconfig.h:1.1
    Rev: src/aclocal.m4:1.1
    Rev: src/configure.in:1.8
    Rev: src/symmetric/Makefile.am:1.1
    Rev: src/symmetric/Makefile.in:1.4(DEAD)
    Rev: src/symmetric/configure.in:1.3(DEAD)

---
+++ src/acconfig.h
@@ -0,0 +1,4 @@
+/* The name and version of this software distribution. These
+ * originates from AM_INIT_AUTOMAKE in the configure.in file. */
+#define PACKAGE "configure-generated"
+#define VERSION "configure-generated"
