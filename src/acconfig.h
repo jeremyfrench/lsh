21d1424f7a8b3e6c4ac9bbc37cbe15c716675137
Niels Möller <nisse@lysator.liu.se>
1998-11-22 03:15:44 +0100

    Configure option --with(out)-debug-alloc.
    Rev: src/acconfig.h:1.2
    Rev: src/configure.in:1.10

--- src/acconfig.h
+++ src/acconfig.h
@@ -2,3 +2,6 @@
  * originates from AM_INIT_AUTOMAKE in the configure.in file. */
 #define PACKAGE "configure-generated"
 #define VERSION "configure-generated"
+
+/* Define to enable sanity checking on memory allocation and casting */
+#undef DEBUG_ALLOC
