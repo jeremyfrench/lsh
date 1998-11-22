3dcc3d8744e9664825f9f34c41ca31b01b62d8d8
Keresztfalvi Gabor Agoston <kg230@hszk.bme.hu>
1998-11-22 01:09:36 +0100

    * lsh.c, lshd.c (main): Bug fix (pass *address* of crypto_rc4_algorithm). Thanks to Keresztfalvi Gabor Agoston <kg230@hszk.bme.hu>.
    Rev: src/lsh.c:1.23

--- src/lsh.c
+++ src/lsh.c
@@ -172,7 +172,7 @@ int main(int argc, char **argv)
   lookup = make_fake_host_db(make_dss_algorithm(NULL)); 
   kex = make_dh_client(dh, lookup);
   algorithms = make_alist(4,
-			  ATOM_ARCFOUR, crypto_rc4_algorithm,
+			  ATOM_ARCFOUR, &crypto_rc4_algorithm,
 			  ATOM_HMAC_SHA1, make_hmac_algorithm(&sha_algorithm),
 			  ATOM_DIFFIE_HELLMAN_GROUP1_SHA1, kex,
 			  ATOM_SSH_DSS, make_dss_algorithm(r), -1);
