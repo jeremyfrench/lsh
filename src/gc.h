95541e01ae74bf2b8d237e74f097fd79a6f391c3
Niels Möller <nisse@lysator.liu.se>
1998-11-26 15:41:52 +0100

    *** empty log message ***
    Rev: src/gc.h:1.1
    Rev: src/make_class:1.1

---
+++ src/gc.h
@@ -0,0 +1,40 @@
+/* gc.h
+ *
+ * Simple mark&sweep garbage collector.
+ *
+ * $Id$ */
+
+/* lsh, an implementation of the ssh protocol
+ *
+ * Copyright (C) 1998 Niels Möller
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef LSH_GC_H_INCLUDED
+#define LSH_GC_H_INCLUDED
+
+#include "lsh_types.h"
+
+void gc_register(struct lsh_object *o);
+int gc(struct lsh_object *root);
+int gc_maybe(struct lsh_object *root, int busy);
+
+#if 0
+void gc_mark(struct lsh_object *o);
+void gc_sweep(void);
+#endif
+
+#endif /* LSH_GC_H_INCLUDED */
