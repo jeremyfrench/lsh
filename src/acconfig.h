b33ca31fffa68922b7116719f58c434bae1b6c85
Niels Möller <nisse@lysator.liu.se>
1998-12-08 20:50:24 +0100

    Added tests for broken shutdown() and for shadow passwords.
    Rev: src/acconfig.h:1.3
    Rev: src/configure.in:1.13

--- src/acconfig.h
+++ src/acconfig.h
@@ -5,3 +5,6 @@
 
 /* Define to enable sanity checking on memory allocation and casting */
 #undef DEBUG_ALLOC
+
+/* Define to indicate that shutdown seems to work properly */
+#undef SHUTDOWN_WORKS_WITH_UNIX_SOCKETS
