18dacb725b956a248dd536b8277734bd720ce6fa
Niels Möller <nisse@lysator.liu.se>
1999-05-23 17:30:27 +0200

    Build packet_types.h
    Rev: misc/bootstrap.sh:1.2

--- misc/bootstrap.sh
+++ misc/bootstrap.sh
@@ -8,4 +8,5 @@ automake -a
 ./configure
 (cd src && for f in *.h *.c; do make $f.x; done)
 rm -f src/*.xT
-(cd src && make atoms_defines.h atoms_gperf.c atoms_table.c prime_table.h sexp_table.h digit_table.h)
+(cd src && make atoms_defines.h atoms_gperf.c atoms_table.c \
+	   prime_table.h sexp_table.h digit_table.h packet_types.h)
