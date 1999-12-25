023e399f544e62a5e3e95620a4c27f5dfc2cc8a4
Niels Möller <nisse@lysator.liu.se>
1999-12-25 22:08:08 +0100

    * acconfig.h: Use more standard #undef's for VERSION and PACKAGE.
    Rev: acconfig.h:1.10

--- acconfig.h
+++ acconfig.h
@@ -1,7 +1,7 @@
 /* The name and version of this software distribution. These
  * originates from AM_INIT_AUTOMAKE in the configure.in file. */
-#define PACKAGE "configure-generated"
-#define VERSION "configure-generated"
+#undef PACKAGE
+#undef VERSION
 
 /* Define to enable sanity checking on memory allocation and casting */
 #undef DEBUG_ALLOC
