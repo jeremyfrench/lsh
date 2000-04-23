419975154231cf814964b27e943b48f43216310d
Niels Möller <nisse@lysator.liu.se>
2000-04-23 19:05:11 +0200

    * src/hmac.c (make_hmac_instance): Added key length argument, and implemented variable size keys.
    Rev: src/hmac.c:1.6

--- src/hmac.c
+++ src/hmac.c
@@ -51,20 +51,22 @@
        (hinner object hash_instance)
        (houter object hash_instance)
 
-       ; Modified by update 
+       ; Modified by update.
        (state object hash_instance)))
 */
 
-static void do_hmac_update(struct mac_instance *s,
-			   UINT32 length, UINT8 *data)
+static void
+do_hmac_update(struct mac_instance *s,
+	       UINT32 length, const UINT8 *data)
 {
   CAST(hmac_instance, self, s);
 
   HASH_UPDATE(self->state, length, data);
 }
 
-static void do_hmac_digest(struct mac_instance *s,
-			   UINT8 *data)
+static void
+do_hmac_digest(struct mac_instance *s,
+	       UINT8 *data)
 {
   CAST(hmac_instance, self, s);
   struct hash_instance *h = self->state;
@@ -79,7 +81,8 @@ static void do_hmac_digest(struct mac_instance *s,
   self->state = HASH_COPY(self->hinner);
 }
 
-static struct mac_instance *do_hmac_copy(struct mac_instance *s)
+static struct mac_instance *
+do_hmac_copy(struct mac_instance *s)
 {
   CAST(hmac_instance, self, s);
   CLONED(hmac_instance, new, self);
@@ -92,12 +95,27 @@ static struct mac_instance *do_hmac_copy(struct mac_instance *s)
 #define IPAD 0x36
 #define OPAD 0x5c
 
-static struct mac_instance *make_hmac_instance(struct mac_algorithm *s,
-					       const UINT8 *key)
+static struct mac_instance *
+make_hmac_instance(struct mac_algorithm *s,
+		   UINT32 key_length,
+		   const UINT8 *key)
 {
   CAST(hmac_algorithm, self, s);
   NEW(hmac_instance, instance);
   UINT8 *pad = alloca(self->hash->block_size);
+
+  if (key_length > self->hash->block_size)
+    {
+      /* Reduce key to the algorithm's hash size */
+      UINT8 *digest = alloca(self->hash->hash_size);
+      struct hash_instance *h = MAKE_HASH(self->hash);
+
+      HASH_UPDATE(h, key_length, key);
+      HASH_DIGEST(h, digest);
+
+      key = digest;
+      key_length = self->hash->hash_size;
+    }
 
   instance->super.hash_size = self->super.hash_size;
   instance->super.update = do_hmac_update;
@@ -107,14 +125,14 @@ static struct mac_instance *make_hmac_instance(struct mac_algorithm *s,
   instance->hinner = MAKE_HASH(self->hash);
 
   memset(pad, IPAD, self->hash->block_size);
-  memxor(pad, key, self->hash->hash_size);
+  memxor(pad, key, key_length);
 
   HASH_UPDATE(instance->hinner, self->hash->block_size, pad);
 
   instance->houter = MAKE_HASH(self->hash);
 
   memset(pad, OPAD, self->hash->block_size);
-  memxor(pad, key, self->hash->hash_size);
+  memxor(pad, key, key_length);
   
   HASH_UPDATE(instance->houter, self->hash->block_size, pad);
 
@@ -123,7 +141,8 @@ static struct mac_instance *make_hmac_instance(struct mac_algorithm *s,
   return &instance->super;
 } 
   
-struct mac_algorithm *make_hmac_algorithm(struct hash_algorithm *h)
+struct mac_algorithm *
+make_hmac_algorithm(struct hash_algorithm *h)
 {
   NEW(hmac_algorithm, self);
 
