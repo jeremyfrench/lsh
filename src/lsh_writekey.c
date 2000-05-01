43541c2290212fd730b1836491dbab563c762211
Niels Möller <nisse@lysator.liu.se>
2000-05-01 22:07:30 +0200

    * src/lsh_writekey.c: Major reorganization. Use argp, and support pkcs5-style encryption.
    Rev: src/lsh_writekey.c:1.18

--- src/lsh_writekey.c
+++ src/lsh_writekey.c
@@ -23,14 +23,20 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "algorithms.h"
 #include "blocking_write.h"
+/* For read_password() */
+#include "password.h"
+#include "crypto.h"
 #include "format.h"
 #include "io_commands.h"
 #include "sexp_commands.h"
-#include "spki.h"
+#include "spki_commands.h"
+#include "version.h"
 #include "werror.h"
 #include "xalloc.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <string.h>
 
@@ -42,11 +48,14 @@
 #include <unistd.h>
 #endif
 
+#define BLOCK_SIZE 2000
+
 static struct read_sexp_command read_sexp
 = STATIC_READ_SEXP(SEXP_TRANSPORT, 0);
 
 #define READ_SEXP (&read_sexp.super.super)
 
+#if 0
 static struct sexp_print_command write_canonical
 = STATIC_PRINT_SEXP(SEXP_CANONICAL);
 #define CANONICAL (&write_canonical.super.super.super)
@@ -54,27 +63,338 @@ static struct sexp_print_command write_canonical
 static struct sexp_print_command write_transport
 = STATIC_PRINT_SEXP(SEXP_TRANSPORT);
 #define TRANSPORT (&write_transport.super.super.super)
+#endif
+
+struct lsh_writekey_options;
+
+extern struct command_simple lsh_writekey_print_public;
+#define PRINT_PUBLIC (&lsh_writekey_print_public.super.super)
+
+extern struct command_simple lsh_writekey_print_private;
+#define PRINT_PRIVATE (&lsh_writekey_print_private.super.super)
+
+extern struct command_simple lsh_writekey_options2algorithms;
+#define OPTIONS2ALGORITHMS (&lsh_writekey_options2algorithms.super.super)
+
+extern struct command_simple lsh_writekey_options2transform;
+#define TRANSFORM (&lsh_writekey_options2transform.super.super)
+
+extern struct command_simple lsh_writekey_options2public_file;
+#define OPTIONS2PUBLIC_FILE (&lsh_writekey_options2public_file.super.super)
+
+extern struct command_simple lsh_writekey_options2private_file;
+#define OPTIONS2PRIVATE_FILE (&lsh_writekey_options2private_file.super.super)
 
 #include "lsh_writekey.c.x"
+
+
+/* Option parsing */
+
+const char *argp_program_version
+= "lsh_writekey-" VERSION;
+
+const char *argp_program_bug_address = BUG_ADDRESS;
+
+/* GABA:
+   (class
+     (name lsh_writekey_options)
+     (vars
+       ; Base filename
+       (file string)
+
+       (label string)
+       (style . sexp_argp_state)
+       (passphrase string)
+
+       (crypto_algorithms object alist)
+       (signature_algorithms object alist)
+       (r object randomness)
+       
+       (crypto_name . UINT32)
+       (crypto object crypto_algorithm)
+       (iterations . UINT32)))
+*/
+
+static struct lsh_writekey_options *
+make_lsh_writekey_options(void)
+{
+  NEW(lsh_writekey_options, self);
+  self->file = NULL;
+  
+  self->label = NULL;
+  self->style = -1;
+
+  self->passphrase = NULL;
+  self->iterations = 1500;
+
+  self->crypto_algorithms = many_algorithms(0, -1);
+  self->signature_algorithms
+    = make_alist(1,
+		 ATOM_DSA, make_dsa_algorithm(NULL), -1);
+  /* We use this only for salt and iv generation. */
+  self->r = make_reasonably_random();
+
+  /* A better default would be crypto_cbc(make_des3()) */
+  self->crypto = NULL;
+  
+  return self;
+}
+
+static const struct argp_option
+main_options[] =
+{
+  /* Name, key, arg-name, flags, doc, group */
+  { "output-file", 'o', "Filename", 0, "Default is ~/.lsh/identity", 0 },
+  { NULL, 'f', NULL, OPTION_ALIAS, NULL, 0 },
+  { "iteration-count", 'i', "PKCS#5 iteration count", 0, "Default is 1500", 0 },
+  { "crypto", 'c', "Algorithm", 0, "Encryption algorithm for the private key file.", 0 },
+  { "label", 'l', "Text", 0, "Unencrypted label for the key.", 0 },
+  { "pass phrase", 'p', "Password", 0, NULL, 0 },
+  { NULL, 0, NULL, 0, NULL, 0 }
+};
+
+static const struct argp_child
+main_argp_children[] =
+{
+  { &sexp_output_argp, 0, NULL, 0 },
+  { &werror_argp, 0, "", 0 },
+  { NULL, 0, NULL, 0}
+};
+
+
+static error_t
+main_argp_parser(int key, char *arg, struct argp_state *state)
+{
+  CAST(lsh_writekey_options, self, state->input);
+
+  switch(key)
+    {
+    default:
+      return ARGP_ERR_UNKNOWN;
+
+    case ARGP_KEY_INIT:
+      state->child_inputs[0] = &self->style;
+      state->child_inputs[1] = NULL;
+      break;
+
+    case ARGP_KEY_END:
+      if (!self->file)
+	{
+	  char *home = getenv("HOME");
+	  struct lsh_string *s;
+	  
+	  if (!home)
+	    {
+	      argp_failure(state, EXIT_FAILURE, 0, "$HOME not set.");
+	      return EINVAL;
+	    }
+	  else
+	    {
+	      s = ssh_format("%lz/.lsh%c", home, 0);
+	      if (mkdir(s->data, 0755) < 0)
+		{
+		  if (errno != EEXIST)
+		    argp_failure(state, EXIT_FAILURE, errno, "Creating directory %s failed.", s->data);
+		}
+	      lsh_string_free(s);
+	      self->file = ssh_format("%lz/.lsh/identity%c", home, 0);
+	    }
+	}
+      if (self->crypto)
+	{
+	  if (!self->label)
+	    {
+	      const char *name = getenv("LOGNAME");
+#ifndef MAXHOSTNAMELEN
+#define MAXHOSTNAMELEN 300
+#endif
+	      char host[MAXHOSTNAMELEN];
+	  
+	      if (!name)
+		{
+		  argp_failure(state, EXIT_FAILURE, 0,
+			       "LOGNAME not set. Please use the -l option.");
+		  return EINVAL;
+		}
+
+	      if ( (gethostname(host, sizeof(host)) < 0)
+		   && (errno != ENAMETOOLONG) )
+		argp_failure(state, EXIT_FAILURE, errno,
+			     "Can't get the host name. Please use the -l option.");
+	      
+	      self->label = ssh_format("%lz@%lz", name, host);
+	    }
+	  while (!self->passphrase)
+	    {
+	      struct lsh_string *pw;
+	      struct lsh_string *again;
+
+	      pw = read_password(500, ssh_format("Enter new passphrase: "), 1);
+	      if (!pw)
+		argp_failure(state, EXIT_FAILURE, 0, "Aborted.");
+
+	      again = read_password(500, ssh_format("Again: "), 1);
+	      if (!again)
+		argp_failure(state, EXIT_FAILURE, 0, "Aborted.");
+
+	      if (lsh_string_eq(pw, again))
+		self->passphrase = pw;
+	      else
+		lsh_string_free(pw);
+
+	      lsh_string_free(again);
+	    }
+	}
+      break;
+      
+    case 'o':
+    case 'f':
+      self->file = format_cstring(arg);
+      break;
+
+    case 'i':
+      {
+	long i;
+	char *end;
+	i = strtol(arg, &end, 0);
+
+	if ((end == arg) || *end || (i < 1))
+	  {
+	    argp_failure(state, EXIT_FAILURE, 0, "Invalid iteration count.");
+	    return EINVAL;
+	  }
+	else if (i > PKCS5_MAX_ITERATIONS)
+	  {
+	    argp_error(state, "Iteration count ridiculously large (> %d).",
+		       PKCS5_MAX_ITERATIONS);
+	    return EINVAL;
+	  }
+	else
+	  self->iterations = i;
+
+	break;
+      }
+
+    case 'c':
+      {
+	int name = lookup_crypto(self->crypto_algorithms, arg, &self->crypto);
+
+	if (name)
+	  self->crypto_name = name;
+	else
+	  {
+	    list_crypto_algorithms(state, self->crypto_algorithms);
+	    argp_error(state, "Unknown crypto algorithm '%s'.", arg);
+	  }
+	break;
+      }
+      
+    case 'l':
+      self->label = ssh_format("%lz", arg);
+      break;
+      
+    case 'p':
+      self->passphrase = ssh_format("%lz", arg);
+      break;
+    }
+  return 0;
+}
+
+static const struct argp
+main_argp =
+{ main_options, main_argp_parser, 
+  NULL,
+  ( "Splits a keypair in one private and one public file, "
+    "optionally encrypting the private file using a passphrase.\v"
+    "Common usage is to pipe the output from lsh_keygen into this program."
+    ),
+  main_argp_children,
+  NULL, NULL
+};
+
+COMMAND_SIMPLE(lsh_writekey_print_public)
+{
+  CAST(lsh_writekey_options, options, a);
+
+  return &make_sexp_print_command
+    ((options->style > 0) ? options->style : SEXP_TRANSPORT)->super.super;
+}
+
+COMMAND_SIMPLE(lsh_writekey_print_private)
+{
+  CAST(lsh_writekey_options, options, a);
+
+  return &make_sexp_print_command
+    ((options->style > 0) ? options->style : SEXP_CANONICAL)->super.super;
+}
+
+COMMAND_SIMPLE(lsh_writekey_options2algorithms)
+{
+  CAST(lsh_writekey_options, options, a);
+  return &options->signature_algorithms->super;
+}
+
+COMMAND_SIMPLE(lsh_writekey_options2transform)
+{
+  CAST(lsh_writekey_options, options, a);
+  if (!options->crypto)
+    return &command_I.super.super;
+  else
+    {
+      CAST_SUBTYPE(mac_algorithm, hmac,
+		   ALIST_GET(options->crypto_algorithms, ATOM_HMAC_SHA1));
+      assert(hmac);
+      
+      return
+	&make_pkcs5_encrypt(options->r,
+			    lsh_string_dup(options->label),
+			    ATOM_HMAC_SHA1,
+			    hmac,
+			    options->crypto_name,
+			    options->crypto,
+			    10, /* Salt length */
+			    lsh_string_dup(options->passphrase),
+			    options->iterations)->super;
+    }
+}
+
+COMMAND_SIMPLE(lsh_writekey_options2public_file)
+{
+  CAST(lsh_writekey_options, options, a);
+  struct lsh_string *public = ssh_format("%lS.pub%c", options->file, 0);
+
+  return
+    &make_io_write_file_info(public->data,
+			     O_CREAT | O_EXCL | O_WRONLY,
+			     0644,
+			     BLOCK_SIZE)->super;
+}
+
+COMMAND_SIMPLE(lsh_writekey_options2private_file)
+{
+  CAST(lsh_writekey_options, options, a);
+  return
+    &make_io_write_file_info(options->file->data,
+			     O_CREAT | O_EXCL | O_WRONLY,
+			     0600,
+			     BLOCK_SIZE)->super;
+}
 
 /* GABA:
    (expr
      (name make_writekey)
      (globals
        (open IO_WRITE_FILE)
-       (stdin IO_READ_STDIN)
-       (read READ_SEXP) )
+       (stdin IO_READ_STDIN))
      (params
-       (private object io_write_file_info)
-       (public object io_write_file_info)
-       (algorithms object alist))
+       (options object lsh_writekey_options))
      (expr
        (lambda (backend)
-         (let ((key (read (stdin backend))))
-           (prog1 (transport (open backend public)
-	                     (signer2public (sexp2signer algorithms key)))
-	          ; FIXME: Add encryption here
-	          (canonical (open backend private) key))))))
+         (let ((key (read_sexp (stdin backend))))
+           (prog1 (print_public options (open backend (options2public_file options))
+	                        (signer2public (sexp2signer (options2algorithms options) key)))
+	          (print_private options (open backend (options2private_file options))
+		                 (transform options key)))))))
 */
 
 static void
@@ -91,82 +411,19 @@ static struct exception_handler exc_handler =
 STATIC_EXCEPTION_HANDLER(do_lsh_writekey_handler, NULL);
 
 
-#define BLOCK_SIZE 2000
-
-static void usage(void)
-{
-  werror("Usage: lsh_writekey [filename]\n");
-}
-
-int main(int argc UNUSED, char **argv UNUSED)
+int main(int argc, char **argv)
 {
   NEW(io_backend, backend);
-
-  /* int status = 1; */
-  char *public;
-  char *private;
+  struct lsh_writekey_options *options = make_lsh_writekey_options();
   
-  switch (argc)
-    {
-    case 1:
-      {
-	char *home = getenv("HOME");
-	char *buf;
-	
-	if (!home)
-	  {
-	    werror("lsh_keygen: $HOME not set.\n");
-	    return EXIT_FAILURE;
-	  }
-	buf = alloca(strlen(home) + 20);
-	sprintf(buf, "%s/.lsh", home);
-	if (mkdir(buf, 0755) < 0)
-	  {
-	    if (errno != EEXIST)
-	      {
-		werror("lsh_writekey: Creating directory %z failed "
-		       "(errno = %i): %z\n", buf, errno, STRERROR(errno));
-		return EXIT_FAILURE;
-	      }
-	  }
-	else
-	  werror("lsh_writekey: Created directory %z\n", buf);
-	
-	sprintf(buf, "%s/.lsh/identity", home);
-	private = buf;
-	
-	break;
-      }
-    case 2:
-      if (argv[1][0] != '-')
-	{
-	  private = argv[1];
-	  break;
-	}
-      /* Fall through */
-    default:
-      usage();
-      return EXIT_FAILURE;
-    }
+  argp_parse(&main_argp, argc, argv, 0, NULL, options);
 
-  public = alloca(strlen(private) + 5);
-  sprintf(public, "%s.pub", private);
-  
   init_backend(backend);
 
   {
     CAST_SUBTYPE
       (command, work,
-       make_writekey(make_io_write_file_info(private,
-					     O_CREAT | O_EXCL | O_WRONLY,
-					     0600,
-					     BLOCK_SIZE),
-		     make_io_write_file_info(public,
-					     O_CREAT | O_EXCL | O_WRONLY,
-					     0644,
-					     BLOCK_SIZE),
-		     make_alist(1,
-				ATOM_DSA, make_dsa_algorithm(NULL), -1)));
+       make_writekey(options));
     
     COMMAND_CALL(work, backend, &discard_continuation, &exc_handler);
   }
