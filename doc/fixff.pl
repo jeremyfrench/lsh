c865be0576e21719fe28abb45e711e78978ae994
Niels Möller <nisse@lysator.liu.se>
2000-08-03 00:44:31 +0200

    *** empty log message ***
    Rev: doc/fixff.pl:1.3
    Rev: doc/srp-spec.nroff:1.4

--- doc/fixff.pl
+++ doc/fixff.pl
@@ -38,7 +38,7 @@ while (<>) {                            # Read the entire input file.
     s/FORMFEED(\[Page\s+\d+\])\s+/        \1\n\f\n/g;
                                         # Rewrite the end-of-pages.
 #    s/\f\n$/\f/;                       # Want bare formfeed at end?
-#    s/\f\n$//;                         # Want no formfeed at end?
+    s/\f\n$//;                          # Want no formfeed at end?
     print;                              # Print the resultant file.
 }
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
