acd5b14d8db5a02dffd75fb7cb701ee7f109b0c5
Niels Möller <nisse@lysator.liu.se>
2000-10-31 23:04:10 +0100

    * src/exception.h (EXC_RANDOMNESS, EXC_RANDOMNESS_LOW_ENTROPY): New exception types.
    Rev: src/exception.h:1.23

--- src/exception.h
+++ src/exception.h
@@ -150,8 +150,11 @@ void exception_raise(struct exception_handler *e,
 #define EXC_PAUSE_CONNECTION 0x100003
 #define EXC_PAUSE_START_CONNECTION 0x100004
 
+#define EXC_RANDOMNESS 0x200000
+#define EXC_RANDOMNESS_LOW_ENTROPY 0x200001
+
 /* Application specific exceptions */
-#define EXC_APP 0x200000
+#define EXC_APP 0x400000
 
 #define EXC_ALL (~0)
 
