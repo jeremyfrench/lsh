2dfe6af74d8cdcc63dc6c8479d122a9a730731c1
Niels Möller <nisse@lysator.liu.se>
2000-11-15 22:40:11 +0100

    * src/service.c, src/service.h: Marked as obsolete. Moved its two functions, format_service_request() and format_service_accept() to the files that use them.
    Rev: src/service.c:1.18
    Rev: src/service.h:1.18

--- src/service.h
+++ src/service.h
@@ -21,6 +21,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#warning service.h is obsolete
 #ifndef LSH_SERVICE_H_INCLUDED
 #define LSH_SERVICE_H_INCLUDED
 
