4aa052f262df5e77cbb5d73b380837ed7236ad25
Niels Möller <nisse@lysator.liu.se>
2001-02-18 23:33:15 +0100

    * src/resource.c (do_remember_resource): If the resource list is dead, immediately kill any resources added to it.
    Rev: src/resource.c:1.13

--- src/resource.c
+++ src/resource.c
@@ -138,7 +138,13 @@ do_remember_resource(struct resource_list *s,
   CAST(concrete_resource_list, self, s);
   struct resource_node *n;
 
-  assert(self->super.super.alive);
+  if (!self->super.super.alive)
+    {
+      werror("do_remember_resource: resource list is already dead.\n");
+      KILL(resource);
+      return;
+    }
+  
   NEW_SPACE(n);
 
   n->resource = resource;
