370ddd76d3b79c4ec1a2999034425ca857da2357
Niels Möller <nisse@lysator.liu.se>
2001-03-14 19:28:49 +0100

    (make_install_new_keys: Deleted.
    Rev: src/keyexchange.h:1.50

--- src/keyexchange.h
+++ src/keyexchange.h
@@ -115,20 +115,23 @@
 struct lsh_string *format_kex(struct kexinit *kex);
 void disconnect_kex_failed(struct ssh_connection *connection, const char *msg);
 
-struct crypto_instance *kex_make_encrypt(struct hash_instance *secret,
-					 struct object_list *algorithms,
-					 int type,
-					 struct lsh_string *session_id);
+struct crypto_instance *
+kex_make_encrypt(struct hash_instance *secret,
+		 struct object_list *algorithms,
+		 int type,
+		 struct lsh_string *session_id);
 
-struct crypto_instance *kex_make_decrypt(struct hash_instance *secret,
-					 struct object_list *algorithms,
-					 int type,
-					 struct lsh_string *session_id);
+struct crypto_instance *
+kex_make_decrypt(struct hash_instance *secret,
+		 struct object_list *algorithms,
+		 int type,
+		 struct lsh_string *session_id);
 
-struct mac_instance *kex_make_mac(struct hash_instance *secret,
-				  struct object_list *algorithms,
-				  int type,
-				  struct lsh_string *session_id);
+struct mac_instance *
+kex_make_mac(struct hash_instance *secret,
+	     struct object_list *algorithms,
+	     int type,
+	     struct lsh_string *session_id);
 
 struct make_kexinit *
 make_simple_kexinit(struct randomness *r,
@@ -153,10 +156,6 @@ struct packet_handler *
 make_newkeys_handler(struct crypto_instance *crypto,
 		     struct mac_instance *mac,
 		     struct compress_instance *compression);
-
-struct install_keys *
-make_install_new_keys(int is_server,
-		      struct object_list *algorithms);
 
 void
 keyexchange_finish(struct ssh_connection *connection,
