a066b0bdd3ff6ced8064217687a0cb3797a739c7
Niels Möller <nisse@lysator.liu.se>
2001-04-01 00:34:20 +0200

    * src/sftp/testsuite/common.sh (PROGRAMDIR): New variable, holding an absolute path to the directory where the programs are.
    Rev: src/sftp/testsuite/common.sh:1.2

--- src/sftp/testsuite/common.sh
+++ src/sftp/testsuite/common.sh
@@ -3,8 +3,19 @@
 set -e
 
 TESTHOME=home
+export TESTHOME
+
+# We need an absolute path.
+PROGRAMDIR=`cd .. && pwd`
+export PROGRAMDIR
+
+if [ -n "$SFTP_DEBUG" ] ; then
+    SERVER=debug-server
+else
+    SERVER=sftp-server
+fi
 
 function client () {
-    cd $TESTHOME ../../sftp-test-client ../../sftp-server "$@"
+    (cd $TESTHOME && "$PROGRAMDIR"/sftp-test-client "$PROGRAMDIR"/"$SERVER" "$@")
 }
     
