d1a529d3bf171184f8ac686474c945e336bdc280
Niels Möller <nisse@lysator.liu.se>
2001-05-01 20:32:57 +0200

    Marked as obsolete.
    Rev: src/sexp_streamed_parser.c:1.30

--- src/sexp_streamed_parser.c
+++ src/sexp_streamed_parser.c
@@ -21,6 +21,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#warning sexp_streamed_parser.c is obsolete.
+
 #include "abstract_io.h"
 #include "command.h"
 #include "digits.h"
