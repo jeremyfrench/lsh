d718cdd37099a9dfbb388ef4bcb17be8595cd909
Niels Möller <nisse@lysator.liu.se>
2001-10-05 09:10:17 +0200

    * src/algorithms.c (all_symmetric_algorithms): Replaced references to rijndael_algorithm with aes256_cbc_algorithm.
    Rev: src/crypto.h:1.24

--- src/crypto.h
+++ src/crypto.h
@@ -50,10 +50,16 @@ extern struct crypto_algorithm twofish128_algorithm;
 extern struct crypto_algorithm twofish192_algorithm;
 extern struct crypto_algorithm twofish256_algorithm;
 
+#if 0
 struct crypto_algorithm *make_rijndael_algorithm(UINT32 key_size);
 extern struct crypto_algorithm rijndael128_algorithm;
 extern struct crypto_algorithm rijndael192_algorithm;
 extern struct crypto_algorithm rijndael256_algorithm;
+#endif
+
+extern struct crypto_algorithm aes128_cbc_algorithm;
+extern struct crypto_algorithm aes192_cbc_algorithm;
+extern struct crypto_algorithm aes256_cbc_algorithm;
 
 struct crypto_algorithm *make_serpent_algorithm(UINT32 key_size);
 extern struct crypto_algorithm serpent128_algorithm;
