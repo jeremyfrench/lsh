dc903b99bee0bf7fd776e34e02c5530344724969
Niels Möller <nisse@lysator.liu.se>
2002-03-05 12:55:41 +0100

    Don't use m4 to generate test programs. Added C files to CVS, deleted m4 files.
    Rev: src/testsuite/aes-test.c:1.1
    Rev: src/testsuite/arcfour-test.c:1.1
    Rev: src/testsuite/blowfish-test.c:1.1
    Rev: src/testsuite/cast128-test.c:1.1
    Rev: src/testsuite/des-test.c:1.1
    Rev: src/testsuite/serpent-test.c:1.1
    Rev: src/testsuite/twofish-test.c:1.1

---
+++ src/testsuite/serpent-test.c
@@ -0,0 +1,15 @@
+#include "testutils.h"
+
+int
+test_main(void)
+{
+  test_cipher("Serpent-256 CBC", &crypto_serpent256_cbc_algorithm,
+              H("0123456789ABCDEF FEDCBA9876543210"
+		"0011223344556677 8899AABBCCDDEEFF"),
+              H("0000000000000000 0000000000000000"
+		"1111111111111111 1111111111111111"),
+	      H("687a151886b6dc16 8d2b667c3b8d5226"
+		"e63a6c8b3009859c 8ee559565befb77e"),
+	      H("0011223344556677 8899AABBCCDDEEFF"));
+  SUCCESS();
+}
