e7d1d8b36391e95d9013646306996a77e7044619
Niels Möller <nisse@lysator.liu.se>
2002-03-27 17:39:37 +0100

    * werror.c (_fatal): New function.
    Rev: src/sftp/werror.c:1.2
    Rev: src/sftp/werror.h:1.2

--- src/sftp/werror.c
+++ src/sftp/werror.c
@@ -11,6 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <unistd.h>
+
 void
 werror(const char *format, ...)
 {
@@ -37,4 +39,19 @@ fatal(const char *format, ...)
   va_end(args);
 
   exit(EXIT_FAILURE);
+}
+
+void
+_fatal(const char *format, ...)
+{
+  va_list args;
+
+  va_start(args, format);
+  
+  fprintf(stderr, "%s: ", werror_program_name);
+  vfprintf(stderr, format, args);
+
+  va_end(args);
+
+  _exit(EXIT_FAILURE);
 }
--- src/sftp/werror.h
+++ src/sftp/werror.h
@@ -15,6 +15,7 @@ extern const char *werror_program_name;
 
 void werror(const char *format, ...) PRINTF_STYLE(1,2);
 void fatal(const char *format, ...) PRINTF_STYLE(1,2) NORETURN;
+void _fatal(const char *format, ...) PRINTF_STYLE(1,2) NORETURN;
 
 #endif /* SFTP_WERROR_H_INCLUDED */
 
