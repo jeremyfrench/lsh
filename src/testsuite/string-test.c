e1097fdd2950eee235d9bcf2c28bacd51b7048ba
Pontus Freyhult <pont@soua.net>
2002-04-03 16:27:47 +0200

    Added tests for lsh_string_colonize.
    Rev: src/testsuite/string-test.c:1.2

--- src/testsuite/string-test.c
+++ src/testsuite/string-test.c
@@ -21,5 +21,29 @@ test_main(void)
   p = lsh_get_cstring(s);
   ASSERT(p && !*p);
 
+  s = S("colonize this");
+  s = lsh_string_colonize(s, 2, 1);
+  p = lsh_get_cstring(s);
+  
+  ASSERT(p && !strcmp(p, "co:lo:ni:ze: t:hi:s")) ;
+
+  s = S("fo");
+  s = lsh_string_colonize(s, 2, 1);
+  p = lsh_get_cstring(s);
+  
+  ASSERT(p && !strcmp(p, "fo")) ;
+ 
+  s = S("");
+  s = lsh_string_colonize(s, 1, 1);
+  p = lsh_get_cstring(s);
+  
+  ASSERT(p && !*p) ;
+  
+  s = S("colonize this");
+  s = lsh_string_colonize(s, 1, 1);
+  p = lsh_get_cstring(s);
+
+  ASSERT(p && !strcmp(p, "c:o:l:o:n:i:z:e: :t:h:i:s")) ;
+  
   SUCCESS();
 }
