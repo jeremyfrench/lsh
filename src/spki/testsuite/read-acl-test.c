8b9c47b62f362d6356c039681b31df7acb9316b9
Niels Möller <nisse@lysator.liu.se>
2002-11-19 02:16:27 +0100

    * testsuite/testutils.c (read_acl): New function. * testsuite/read-acl-test.c (test_main): Use it.
    Rev: src/spki/testsuite/read-acl-test.c:1.5

--- src/spki/testsuite/read-acl-test.c
+++ src/spki/testsuite/read-acl-test.c
@@ -4,21 +4,16 @@ void
 test_main(void)
 {
   struct spki_acl_db db;
-  struct spki_iterator i;
   
   spki_acl_init(&db);
 
-  ASSERT(spki_iterator_first
-	 (&i, LDATA
-	  ("(3:acl(7:version1:\0)(5:entry"
-	   "(4:hash3:md516:xxxxxxxxxxxxxxxx)"
-	   "(3:tag(3:ftp18:ftp.lysator.liu.se)))"
-	   "(5:entry(10:public-key2:k1)"
-	   "(3:tag(4:http32:http://www.lysator.liu.se/~nisse4:read))))"))
-	 == SPKI_TYPE_ACL);
+  read_acl(&db, LDATA
+	   ("(3:acl(7:version1:\0)(5:entry"
+	    "(4:hash3:md516:xxxxxxxxxxxxxxxx)"
+	    "(3:tag(3:ftp18:ftp.lysator.liu.se)))"
+	    "(5:entry(10:public-key2:k1)"
+	    "(3:tag(4:http32:http://www.lysator.liu.se/~nisse4:read))))"));
   
-  ASSERT(spki_acl_parse(&db, &i));
-
   ASSERT(spki_principal_by_key(&db, LDATA("k1")));
   ASSERT(spki_principal_by_md5(&db, "xxxxxxxxxxxxxxxx"));
 }
