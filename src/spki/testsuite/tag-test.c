8a5d77a1c761c99fc4c392e2d1dcd1c38f861cef
Niels Möller <nisse@lysator.liu.se>
2002-11-22 10:57:56 +0100

    (test_main): Added more tests.
    Rev: src/spki/testsuite/tag-test.c:1.2

--- src/spki/testsuite/tag-test.c
+++ src/spki/testsuite/tag-test.c
@@ -20,19 +20,50 @@ includes(unsigned alength, const uint8_t *adata,
     return 0;
 }
 
+#define INCLUDES(a, b) includes(LDATA(a), LDATA(b))
+
 void
 test_main(void)
 {
-  ASSERT(includes(LDATA("(3:ftp18:ftp.lysator.liu.se)"),
-		  LDATA("(3:ftp18:ftp.lysator.liu.se)")));
+  ASSERT(INCLUDES("(3:ftp18:ftp.lysator.liu.se)",
+		  "(3:ftp18:ftp.lysator.liu.se)"));
 
-  ASSERT(!includes(LDATA("(3:ftp18:ftp.lysator.liu.se)"),
-		   LDATA("(4:http18:ftp.lysator.liu.se)")));
+  ASSERT(!INCLUDES("(3:ftp18:ftp.lysator.liu.se)",
+		   "(4:http18:ftp.lysator.liu.se)"));
   
 
-  ASSERT(includes(LDATA("(3:ftp18:ftp.lysator.liu.se)"),
-		  LDATA("(3:ftp18:ftp.lysator.liu.se4:read)")));
+  ASSERT(INCLUDES("(3:ftp18:ftp.lysator.liu.se)",
+		  "(3:ftp18:ftp.lysator.liu.se4:read)"));
 
-  ASSERT(!includes(LDATA("(3:ftp18:ftp.lysator.liu.se4:read)"),
-		   LDATA("(3:ftp18:ftp.lysator.liu.se)")));
+  ASSERT(!INCLUDES("(3:ftp18:ftp.lysator.liu.se4:read)",
+		   "(3:ftp18:ftp.lysator.liu.se)"));
+
+  ASSERT(INCLUDES("(1:*)", "(3:foo)"));
+  ASSERT(!INCLUDES("(3:foo)", "(1:*)"));
+
+  ASSERT(INCLUDES("(1:*)",
+		  "(1:*3:set3:foo3:bar)"));
+  ASSERT(!INCLUDES("(1:*3:set3:foo3:bar)",
+		   "(1:*)"));
+
+  ASSERT(INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		  "(3:foo)"));
+  ASSERT(INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		  "(3:foo4:read)"));
+  ASSERT(INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		  "(3:bar)"));
+  ASSERT(!INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		   "(3:baz)"));
+
+  /* Set inclusions not implemented. */
+#if 0
+  ASSERT(INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		  "(1:*3:set(3:foo))"));
+  ASSERT(INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		  "(1:*3:set(3:foo)(3:bar))"));
+  ASSERT(!INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		   "(1:*3:set(3:foo)(3:bar)(3:baz))"));
+  ASSERT(!INCLUDES("(1:*3:set(3:foo)(3:bar))",
+		   "(1:*3:set(3:foo)(3:baz))"));
+#endif
 }
