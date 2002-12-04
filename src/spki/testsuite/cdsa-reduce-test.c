6f8beacc3e2b42eced1d40019dbb2ea6b058291f
Niels Möller <nisse@lysator.liu.se>
2002-12-04 10:16:24 +0100

    (filter_by_tag, filter_by_subject) (filter_by_date): New functions. (test_main): Process the red-test filter expressions. Omit some tests that seem broken, or use features that we haven't implemented yet. Don't ASSERT that the resulting 5-tuple list is non-empty, some test cases result in empty lists.
    Rev: src/spki/testsuite/cdsa-reduce-test.c:1.2

--- src/spki/testsuite/cdsa-reduce-test.c
+++ src/spki/testsuite/cdsa-reduce-test.c
@@ -55,6 +55,116 @@ read_file(const char *srcdir,
     }
 }
 
+/* Destructively filters the list */
+static struct spki_5_tuple *
+filter_by_tag(struct spki_acl_db *db,
+	      struct spki_5_tuple *acl,
+	      struct spki_iterator *iterator)
+{
+  struct spki_tag *tag;
+  struct spki_5_tuple **pp;
+  struct spki_5_tuple *p;
+  
+  ASSERT(spki_parse_tag(db, iterator, &tag));
+
+  for (pp = &acl; (p = *pp); )
+    {
+      struct spki_tag *intersection
+	= spki_tag_intersect(db->realloc_ctx, db->realloc,
+			     tag, p->tag);
+
+      if (intersection)
+	{
+	  spki_tag_release(db->realloc_ctx, db->realloc, p->tag);
+	  p->tag = intersection;
+
+	  pp = &p->next;
+	}
+      else
+	{
+	  /* Unlink element */
+	  *pp = p->next;
+	  p->next = NULL;
+	  spki_5_tuple_free_chain(db, p);
+	}
+    }
+  return acl;
+}
+
+static struct spki_5_tuple *
+filter_by_subject(struct spki_acl_db *db,
+		  struct spki_5_tuple *acl,
+		  struct spki_iterator *iterator)
+{
+  struct spki_principal *subject;
+  struct spki_5_tuple **pp;
+  struct spki_5_tuple *p;
+
+  ASSERT(spki_parse_principal(db, iterator, SPKI_TYPE_SUBJECT, &subject));
+
+  for (pp = &acl; (p = *pp); )
+    {
+      if (subject == spki_principal_normalize(p->subject))
+	pp = &p->next;
+      
+      else
+	{
+	  /* Unlink element */
+	  *pp = p->next;
+	  p->next = NULL;
+	  spki_5_tuple_free_chain(db, p);
+	}
+    }
+  return acl;
+}
+
+static struct spki_5_tuple *
+filter_by_date(struct spki_acl_db *db,
+	       struct spki_5_tuple *acl,
+	       struct spki_iterator *iterator)
+{
+  struct spki_date not_before;
+  struct spki_date not_after;
+  struct spki_5_tuple **pp;
+  struct spki_5_tuple *p;
+
+  if (iterator->sexp.type == SEXP_ATOM)
+    {
+      /* We have only a single date */
+      ASSERT(spki_parse_date(iterator, &not_before));
+      not_after = not_before;
+    }
+  else
+    {
+      struct spki_5_tuple tuple;
+      ASSERT(spki_parse_valid(iterator, &tuple));
+      not_before = tuple.not_before;
+      not_after = tuple.not_after;
+    }
+      
+      
+  for (pp = &acl; (p = *pp); )
+    {
+      if (SPKI_DATE_CMP(p->not_before, not_before) < 0)
+	p->not_before = not_before;
+
+      if (SPKI_DATE_CMP(p->not_after, not_after) > 0)
+	p->not_after = not_after;
+      
+      if (SPKI_DATE_CMP(p->not_before, p->not_after) <= 0)
+	pp = &p->next;
+	  
+      else
+	{
+	  /* Unlink element */
+	  *pp = p->next;
+	  p->next = NULL;
+	  spki_5_tuple_free_chain(db, p);
+	}
+    }
+  return acl;
+}
+
 void
 test_main(void)
 {
@@ -64,9 +174,20 @@ test_main(void)
   for (i = 1; i <= 91; i++)
     {
       struct spki_acl_db db;      
-      struct spki_5_tuple *sequence;
       struct spki_5_tuple *result;
 
+      if (i == 13 /* This test uses an acl with empty tag. Skip it */
+	  || i == 18 /* This tests uses a validity filter with dates missing
+		      * seconds. */
+	  || i == 19
+	  || i == 20 /* This tests uses a validity filter with microseconds. */
+	  )
+	continue;
+
+      if (i == 27)
+	/* Rest of the test cases use features that haven't been
+	 * implemented yet. */
+	break;
       {
 	struct sexp_iterator sexp;
 	struct spki_iterator iterator;
@@ -87,20 +208,49 @@ test_main(void)
 	/* A "red-test" contains an acl and a sequence */
 	spki_acl_init(&db);
 	ASSERT(spki_acl_parse(&db, &iterator));
-	
-	sequence = spki_process_sequence_no_signatures(&db, &iterator);
-	ASSERT(sequence);
+
+	if (iterator.type == SPKI_TYPE_SEQUENCE)
+	  {
+	    struct spki_5_tuple *sequence
+	      = spki_process_sequence_no_signatures(&db, &iterator);
+	    ASSERT(sequence);
+	    result = spki_5_tuple_reduce(&db, sequence);
+
+	    spki_5_tuple_free_chain(&db, sequence);
+	  }
+	else
+	  {
+	    /* Just use the ACL:s */
+	    result = db.first_acl;
+	    db.first_acl = NULL;
+	  }
+
+	if (iterator.type == SPKI_TYPE_TAG)
+	  result = filter_by_tag(&db, result, &iterator);
+
+	ASSERT(result);
+
+	if (iterator.type == SPKI_TYPE_SUBJECT)
+	  result = filter_by_subject(&db, result, &iterator);
+
+	ASSERT(result);
+
+	if (iterator.type == SPKI_TYPE_VALID)
+	  result = filter_by_date(&db, result, &iterator);
 	
 	/* Done with the input file. */
 	free(data);
       }
-      result = spki_5_tuple_reduce(&db, sequence);
+
+#if 0
+      /* At least case 21 results in an empty acl list */
       ASSERT(result);
+#endif
 
       /* The result should be an acl. */
-      assert(!result->issuer);
+      if (result)
+	ASSERT(!result->issuer);
       
-      spki_5_tuple_free_chain(&db, sequence);
       spki_5_tuple_free_chain(&db, result);
 
       spki_acl_clear(&db);
