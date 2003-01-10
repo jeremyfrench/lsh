96737f05afc67c5d322b03f57bd3634d632b7303
Niels Möller <nisse@lysator.liu.se>
2003-01-10 16:43:02 +0100

    (test_main): Include the leading "(tag ..." in the tested expressions.
    Rev: src/testsuite/spki-tag-test.c:1.2

--- src/testsuite/spki-tag-test.c
+++ src/testsuite/spki-tag-test.c
@@ -5,16 +5,16 @@ test_main(void)
 {
   /* Examples taken from RFC-2693 */
   test_spki_grant("1",
-		  S("(3:ftp13:ftp.clark.net3:cme(1:*3:set4:read5:write))"),
-		  S("(3:ftp13:ftp.clark.net3:cme4:read)"));
+		  S("(3:tag(3:ftp13:ftp.clark.net3:cme(1:*3:set4:read5:write)))"),
+		  S("(3:tag(3:ftp13:ftp.clark.net3:cme4:read))"));
 
   test_spki_deny("2",
-		 S("(3:ftp13:ftp.clark.net3:cme(1:*3:set4:read5:write))"),
-		 S("(3:ftp13:ftp.clark.net3:cme6:delete)"));
+		 S("(3:tag(3:ftp13:ftp.clark.net3:cme(1:*3:set4:read5:write)))"),
+		 S("(3:tag(3:ftp13:ftp.clark.net3:cme6:delete))"));
 
   test_spki_deny("3",
-		 S("(3:ftp13:ftp.clark.net3:cme(1:*3:set4:read5:write))"),
-		 S("(3:ftp13:ftp.clark.net3:cme)"));
+		 S("(3:tag(3:ftp13:ftp.clark.net3:cme(1:*3:set4:read5:write)))"),
+		 S("(3:tag(3:ftp13:ftp.clark.net3:cme))"));
 
   SUCCESS();
 }
