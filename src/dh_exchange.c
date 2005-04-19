f2147731a909743f214a3dac7fdf29f939f5a33d
Niels Möller <nisse@lysator.liu.se>
2005-04-19 23:39:18 +0200

    Reorganized, to fit with new transport code. Server-specific code moved to server_keyexchange; the same should be done tot he client-specific code, but that's not done yet.
    Rev: src/dh_exchange.c:1.20.2.2

--- src/dh_exchange.c
+++ src/dh_exchange.c
@@ -40,123 +40,59 @@
 #include "werror.h"
 #include "xalloc.h"
 
+
 void
-init_dh_instance(const struct dh_method *m,
-		 struct dh_instance *self,
-		 struct kexinit_state *kex)
+init_dh_params(struct dh_params *params,
+	       const char *modulo, unsigned generator,
+	       const struct hash_algorithm *H)
 {
-  struct lsh_string *s;
-  /* FIXME: The allocator could do this kind of initialization
-   * automatically. */
-  mpz_init(self->e);
-  mpz_init(self->f);
-  mpz_init(self->secret);
-
-  self->K = NULL;
-  
-  self->method = m;
-  self->hash = make_hash(m->H);
-  self->exchange_hash = NULL;
-
-  debug("init_dh_instance\n"
-	" V_C: %pS\n", kex->version[CONNECTION_CLIENT]);
-  debug(" V_S: %pS\n", kex->version[CONNECTION_SERVER]);
-  debug(" I_C: %xS\n", kex->literal_kexinit[CONNECTION_CLIENT]);
-  debug(" I_S: %xS\n", kex->literal_kexinit[CONNECTION_SERVER]);
-
-  s = ssh_format("%S%S%S%S",
-		 kex->version[CONNECTION_CLIENT],
-		 kex->version[CONNECTION_SERVER],
-		 kex->literal_kexinit[CONNECTION_CLIENT],
-		 kex->literal_kexinit[CONNECTION_SERVER]);
-  hash_update(self->hash, STRING_LD(s));
-
-  lsh_string_free(s);  
+  mpz_init_set_str(params->modulo, modulo, 16);
+  mpz_init_set_ui(params->generator, generator);
+  params->H = H;
+  params->limit = nettle_mpz_sizeinbase_256_u(params->modulo);
 }
 
-struct dh_method *
-make_dh(const struct zn_group *G,
-	const struct hash_algorithm *H,
-	struct randomness *r)
-{
-  NEW(dh_method, res);
-
-  assert(r->quality == RANDOM_GOOD);
-  
-  res->G = G;
-  res->H = H;
-  res->random = r;
-  
-  return res;
-  
-}
-
-struct dh_method *
-make_dh1(struct randomness *r)
-{
-  return make_dh(make_ssh_group1(), &crypto_sha1_algorithm, r);
-}
-
-struct dh_method *
-make_dh14(struct randomness *r)
-{
-  return make_dh(make_ssh_group14(), &crypto_sha1_algorithm, r);
-}
-
-/* R is set to a random, secret, exponent, and V set to is g^r */
+/* The group for diffie-hellman-group1-sha1, also "Well known group 2"
+   in RFC 2412. */
 void
-dh_generate_secret(const struct dh_method *self,
-		   mpz_t r, mpz_t v)
+init_dh_group1(struct dh_params *params,
+	       const struct hash_algorithm *H)
 {
-  mpz_t tmp;
-
-  /* Generate a random number, 1 < x < O(G) = (p-1)/2 */
-  mpz_init_set(tmp, self->G->order);  
-  mpz_sub_ui(tmp, tmp, 1);
-  nettle_mpz_random(r, self->random, lsh_random, tmp);
-  mpz_add_ui(r, r, 1);
-  mpz_clear(tmp);
-
-  zn_exp(self->G, v, self->G->generator, r);
+  /* 2^1024 - 2^960 - 1 + 2^64 * { [2^894 pi] + 129093 } */  
+  init_dh_params(params,
+		 "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1"
+		 "29024E088A67CC74020BBEA63B139B22514A08798E3404DD"
+		 "EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245"
+		 "E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED"
+		 "EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381"
+		 "FFFFFFFFFFFFFFFF",
+		 2, H);
 }
 
-struct lsh_string *
-dh_make_client_msg(struct dh_instance *self)
+/* The group for diffie-hellman-group14-sha1, also "Well known group
+   14" in RFC 3526. */
+void
+init_dh_group14(struct dh_params *params,
+		const struct hash_algorithm *H)
 {
-  dh_generate_secret(self->method, self->secret, self->e);
-  return ssh_format("%c%n", SSH_MSG_KEXDH_INIT, self->e);
-}
-
-int
-dh_process_client_msg(struct dh_instance *self,
-		      struct lsh_string *packet)
-{
-  struct simple_buffer buffer;
-  unsigned msg_number;
-  mpz_t tmp;
-  
-  simple_buffer_init(&buffer, STRING_LD(packet));
-
-  if (! (parse_uint8(&buffer, &msg_number)
-	 && (msg_number == SSH_MSG_KEXDH_INIT)
-	 && parse_bignum(&buffer, self->e, 0)
-	 && (mpz_cmp_ui(self->e, 1) > 0)
-	 && zn_range(self->method->G, self->e)
-	 && parse_eod(&buffer) ))
-    return 0;
-
-  mpz_init(tmp);
-  
-  zn_exp(self->method->G, tmp, self->e, self->secret);
-  self->K = ssh_format("%ln", tmp);
-
-  mpz_clear(tmp);
-  
-  return 1;
+  /* 2^2048 - 2^1984 - 1 + 2^64 * { [2^1918 pi] + 124476 } */
+  init_dh_params(params,
+		 "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1"
+		 "29024E088A67CC74020BBEA63B139B22514A08798E3404DD"
+		 "EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245"
+		 "E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED"
+		 "EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D"
+		 "C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F"
+		 "83655D23DCA3AD961C62F356208552BB9ED529077096966D"
+		 "670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B"
+		 "E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9"
+		 "DE2BCBF6955817183995497CEA956AE515D2261898FA0510"
+		 "15728E5A8AACAA68FFFFFFFFFFFFFFFF",
+		 2, H);
 }
 
 void
-dh_hash_update(struct dh_instance *self,
+dh_hash_update(struct dh_state *self,
 	       struct lsh_string *s, int free)
 {
   debug("dh_hash_update: %xS\n", s);
@@ -168,7 +104,7 @@ dh_hash_update(struct dh_instance *self,
 
 /* Hashes e, f, and the shared secret key */
 void
-dh_hash_digest(struct dh_instance *self)
+dh_hash_digest(struct dh_state *self)
 {
   dh_hash_update(self, ssh_format("%n%n%S",
 				  self->e, self->f,
@@ -179,26 +115,61 @@ dh_hash_digest(struct dh_instance *self)
 }
 
 void
-dh_make_server_secret(struct dh_instance *self)
+init_dh_state(struct dh_state *self,
+	      const struct dh_params *params,
+	      struct kexinit_state *kex)
 {
-  dh_generate_secret(self->method, self->secret, self->f);
+  mpz_init(self->e);
+  mpz_init(self->f);
+  mpz_init(self->secret);
+
+  self->K = NULL;
+  
+  self->params = params;
+  self->hash = make_hash(params->H);
+  self->exchange_hash = NULL;
+
+  debug("init_dh_instance\n"
+	" V_C: %pS\n", kex->version[0]);
+  debug(" V_S: %pS\n", kex->version[1]);
+  debug(" I_C: %xS\n", kex->literal_kexinit[0]);
+  debug(" I_S: %xS\n", kex->literal_kexinit[1]);
+
+  dh_hash_update(self,
+		 ssh_format("%S%S%S%S",
+			    kex->version[0],
+			    kex->version[1],
+			    kex->literal_kexinit[0],
+			    kex->literal_kexinit[1]),
+		 1);
 }
 
-struct lsh_string *
-dh_make_server_msg(struct dh_instance *self,
-		   struct lsh_string *server_key,
-		   int hostkey_algorithm,
-		   struct signer *s)
+/* R is set to a random, secret, exponent, and V set to is g^r */
+void
+dh_generate_secret(const struct dh_params *self,
+		   struct randomness *random, 
+		   mpz_t r, mpz_t v)
 {
-  dh_hash_update(self, ssh_format("%S", server_key), 1);
-  dh_hash_digest(self);
+  mpz_t tmp;
 
-  return ssh_format("%c%S%n%fS",
-		    SSH_MSG_KEXDH_REPLY,
-		    server_key,
-		    self->f, SIGN(s, hostkey_algorithm,
-				  lsh_string_length(self->exchange_hash),
-				  lsh_string_data(self->exchange_hash)));
+  assert(random->quality == RANDOM_GOOD);
+  
+  /* Generate a random number, 1 < x < O(G) = (p-1)/2 */
+  mpz_init_set(tmp, self->modulo);  
+  mpz_sub_ui(tmp, tmp, 2);
+  nettle_mpz_random(r, random, lsh_random, tmp);
+  mpz_add_ui(r, r, 1);
+  mpz_clear(tmp);
+
+  mpz_powm(v, self->generator, r, self->modulo);
+}
+
+#if 0
+struct lsh_string *
+dh_make_client_msg(struct dh_instance *self)
+{
+  dh_generate_secret(self->method, self->secret, self->e);
+  return ssh_format("%c%n", SSH_MSG_KEXDH_INIT, self->e);
 }
 
 /* Returns the host key. */
@@ -245,3 +216,4 @@ dh_process_server_msg(struct dh_instance *self,
   return key;
 }
 
+#endif
