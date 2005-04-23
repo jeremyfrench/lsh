11a9e9091bc94b8575241bceeb2c0b9da30ace51
Niels Möller <nisse@lysator.liu.se>
2005-04-23 16:45:14 +0200

    Updated to new ssh_write_state interface. (make_ignore_packet): New function. (transport_write_packet): Generate SSH_MSG_IGNORE packets. (transport_write_flush): Likewise.
    Rev: src/transport_write.c:1.1.2.4

--- src/transport_write.c
+++ src/transport_write.c
@@ -32,39 +32,32 @@
 #include "transport.h"
 
 #include "crypto.h"
+#include "format.h"
 #include "lsh_string.h"
 #include "ssh.h"
-#include "ssh_write.h"
 #include "xalloc.h"
 
-#include "transport_write.c.x"
+/* When we have to send a small (less than super.threshold) amount of
+   data, the size is increased by padding with an SSH_MSG_IGNORE
+   message. A single keystroke gives rise to a 10-byte payload, which
+   will be encapsulated and padded to 24 bytes or 32 bytes, depending
+   on the block size, and then the MAC (typically 16 or 20 bytes) is
+   added to this, resulting in a packet in the range 40-52 bytes.
 
-/* GABA:
-   (class
-     (name transport_write_state)
-     (super ssh_write_state)
-     (vars     
-       (mac object mac_instance)
-       (crypto object crypto_instance)
-       (deflate object compress_instance)
-       (seqno . uint32_t)))
-*/
+   A small padding packet should be sufficient to get to a write size
+   that is a multiple of 64 bytes. */
+#define TRANSPORT_PADDING_SIZE 10
 
-/* When we have to send less than TRANSPORT_SMALL_PACKET, size is
-   increased by padding with an SSH_MSG_IGNORE message. A single
-   keystroke gives rise to a 10-byte payload, which will be
-   encapsulated and padded to 24 bytes or 32 bytes, depending on the
-   block size, and then the MAC (typically 16 or 20 bytes) is added to
-   this, resulting in a packet in the range 40-52 bytes. */
-#define TRANSPORT_SMALL_PACKET 60
+#define TRANSPORT_THRESHOLD 500
 
-#define TRANSPORT_MAX_BUFFER (10 * (SSH_MAX_PACKET + SSH_MAX_PACKET_FUZZ))
+#define TRANSPORT_BUFFER_SIZE (10 * (SSH_MAX_PACKET + SSH_MAX_PACKET_FUZZ))
 
 struct transport_write_state *
 make_transport_write_state(void)
 {
   NEW(transport_write_state, self);
-  init_ssh_write_state(&self->super);
+  init_ssh_write_state(&self->super,
+		       TRANSPORT_BUFFER_SIZE, TRANSPORT_THRESHOLD);
   self->mac = NULL;
   self->crypto = NULL;
   self->deflate = NULL;
@@ -73,16 +66,16 @@ make_transport_write_state(void)
   return self;
 }
 
-/* Returns 1 on success, -1 on i/o error, -2 on buffer overflow, and 0
-   if data is still buffered. */
-int
-transport_write_packet(struct transport_write_state *self, int fd, int flush,
-		       struct lsh_string *packet, struct randomness *random)
+static struct lsh_string *
+make_ignore_packet(struct transport_write_state *self,
+		   uint32_t length, struct randomness *random)
 {
-  uint32_t length;
-  int res;
+  uint32_t pad_start;      
+  struct lsh_string *packet;
+  
+  packet = ssh_format("%c%r", SSH_MSG_IGNORE, length, &pad_start);
+  lsh_string_write_random(packet, pad_start, random, length);
 
-  assert(lsh_string_length(packet) > 0);
   packet = encrypt_packet(packet,
 			  self->deflate,
 			  self->crypto,
@@ -90,66 +83,80 @@ transport_write_packet(struct transport_write_state *self, int fd, int flush,
 			  random,
 			  self->seqno++);
 
-#if 0
-  length = lsh_string_length(packet);
-  
-  if (flush && self->crypto
-      && (length + self->super.size) < TRANSPORT_SMALL_PACKET)
-    {
-      uint8_t pad[TRANSPORT_SMALL_PACKET];
-      uint32_t pad_length;
-      uint32_t overhead_length;
-      
-      res = ssh_write(&self->super, fd, 0, packet);      
-      if (res < 0)
-	return res;
-
-      assert(self->super.size < TRANSPORT_SMALL_PACKET);
-
-      /* Desired packet length */
-      pad_length = TRANSPORT_SMALL_PACKET - self->super.size;
-      overhead_length = 14 + (self->mac ? self->mac->mac_size : 0);
-
-      if (pad_length > overhead_length)
-	{
-	  pad_length -= overhead_length;
-	  ASSERT(pad_length < sizeof(pad));
-	  RANDOM(random, pad_length, pad);
-	  packet = ssh_format("%c%s", SSH_MSG_IGNORE, pad_length, pad);
-	}
-      else
-	packet = ssh_format("%c", SSH_MSG_IGNORE);
-
-      packet = encrypt_packet(packet,
-			      self->deflate,
-			      self->crypto,
-			      self->mac,
-			      random,
-			      self->seqno++);
-
-      /* Won't quite work; we need more precise control over which
-	 part of the packet is flushed. */      
-    }
-#endif
-  res = ssh_write_data(&self->super, fd, flush, packet);
-  if (res == 0 && self->super.size > TRANSPORT_MAX_BUFFER)
-    return -2;
-  else
-    return res;
+  return packet;
 }
 
-int
+enum ssh_write_status
+transport_write_packet(struct transport_write_state *self,
+		       int fd, enum ssh_write_flag flags,
+		       struct lsh_string *packet, struct randomness *random)
+{
+  uint32_t length;
+  enum ssh_write_status status;
+
+  assert(lsh_string_length(packet) > 0);
+  if (lsh_string_data(packet)[0] == SSH_MSG_IGNORE)
+    flags |= SSH_WRITE_FLAG_IGNORE;
+
+  packet = encrypt_packet(packet,
+			  self->deflate,
+			  self->crypto,
+			  self->mac,
+			  random,
+			  self->seqno++);
+
+  length = lsh_string_length(packet);
+  
+  if ( (flags == SSH_WRITE_FLAG_PUSH) && self->crypto
+       && (length + self->super.length) < self->super.threshold)
+    {
+      status = ssh_write_data(&self->super, fd, 0, STRING_LD(packet));
+      lsh_string_free(packet);
+
+      if (status < 0)
+	return status;
+
+      packet = make_ignore_packet(self, TRANSPORT_PADDING_SIZE, random);
+
+      flags |= SSH_WRITE_FLAG_IGNORE;
+    }
+
+  status = ssh_write_data(&self->super, fd, flags, STRING_LD(packet));
+  lsh_string_free(packet);
+
+  return status;
+}
+
+enum ssh_write_status
 transport_write_line(struct transport_write_state *self,
 		     int fd,
 		     struct lsh_string *line)
 {
-  return ssh_write_data(&self->super, fd, 0, line);
+  enum ssh_write_status status;
+  status = ssh_write_data(&self->super, fd, 0, STRING_LD(line));
+  lsh_string_free(line);
+  return status;
 }
 
-int
+enum ssh_write_status
 transport_write_flush(struct transport_write_state *self,
-		      int fd)
+		      int fd, struct randomness *random)
 {
+  if (!self->super.ignore && self->crypto
+      && self->super.length < self->super.threshold)
+    {
+      enum ssh_write_status status;
+      struct lsh_string *packet;
+
+      packet = make_ignore_packet(self, TRANSPORT_PADDING_SIZE, random);
+      status = ssh_write_data(&self->super, fd,
+			      SSH_WRITE_FLAG_IGNORE, STRING_LD(packet));
+      lsh_string_free(packet);
+
+      if (status < 0)
+	return status;
+    }
+      
   return ssh_write_flush(&self->super, fd);
 }
 
