7797e6b15eb02a15d9f87f681651c1fae8d5cfa5
Niels Möller <nisse@lysator.liu.se>
2005-09-15 09:41:20 +0200

    (class client_session): Renamed, was client_session_channel. Use channel_read_state and channel_write_state instead of lsh_fd objects. (do_kill_client_session): New method. (oop_write_stdout, oop_write_stderr): New functions. (do_client_session_close): Deleted method. (do_client_session_eof): Updated to use channel_write_state_close. (do_receive): Use channel_io_write. (oop_read_stdin): New function. (do_send_adjust): Use channel_io_start_read. (command client_start_io): Rewritten. (make_client_session_channel): Don't set up a close handler. Rewritten i/o setup.
    Rev: src/client_session.c:1.21.2.1

--- src/client_session.c
+++ src/client_session.c
@@ -4,7 +4,7 @@
 
 /* lsh, an implementation of the ssh protocol
  *
- * Copyright (C) 1998, 1999, 2000 Niels Möller
+ * Copyright (C) 1998, 1999, 2000, 2005 Niels Möller
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -27,28 +27,35 @@
 
 #include <assert.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "client.h"
 
 #include "channel_commands.h"
+#include "channel_io.h"
 #include "client.h"
 #include "io.h"
-#include "read_data.h"
+#include "lsh_string.h"
 #include "ssh.h"
+#include "ssh_write.h"
 #include "werror.h"
 #include "xalloc.h"
 
 #include "client_session.c.x"
 
+
 /* Initiate and manage a session */
 /* GABA:
    (class
-     (name client_session_channel)
+     (name client_session)
      (super ssh_channel)
      (vars
-       ; To access stdio
-       (in object lsh_fd)
-       (out object lsh_fd)
-       (err object lsh_fd)
+       ; Session stdio. The fd:s should be distinct, for simplicity in the close logic.
+       (in struct channel_read_state)
+       (out struct channel_write_state)
+       (err struct channel_write_state)
 
        ; Escape char handling
        (escape object escape_info)
@@ -56,45 +63,123 @@
        (exit_status . "int *")))
 */
 
-/* Callback used when the server sends us eof */
+static void
+do_kill_client_session(struct resource *s)
+{  
+  CAST(client_session, self, s);
+  if (self->super.super.alive)
+    {
+      trace("do_kill_client_session\n");
 
+      self->super.super.alive = 0;
+
+      /* Doesn't use channel_write_state_close, since the channel is
+	 supposedly dead already. */
+      channel_read_state_close(&self->in);
+
+      io_close_fd(self->out.fd);
+      self->out.fd = -1;
+
+      io_close_fd(self->err.fd);
+      self->err.fd = -1;
+
+      channel_pending_close(self->super.table);
+    }
+}
+
+/* Callback used when the server sends us eof */
 static void
 do_client_session_eof(struct ssh_channel *c)
 {
-  CAST(client_session_channel, session, c);
+  CAST(client_session, session, c);
 
-  close_fd_nicely(session->out);
-  close_fd_nicely(session->err);
+  if (!session->out.state->length)
+    channel_write_state_close(&session->super, &session->out);
+
+  if (!session->err.state->length)
+    channel_write_state_close(&session->super, &session->err);
 }  
 
-static void
-do_client_session_close(struct ssh_channel *c)
+static void *
+oop_write_stdout(oop_source *source UNUSED,
+		 int fd, oop_event event, void *state)
 {
-  static const struct exception finish_exception
-    = STATIC_EXCEPTION(EXC_FINISH_PENDING, "Session closed.");
+  CAST(client_session, session, (struct lsh_object *) state);
 
-  EXCEPTION_RAISE(c->e, &finish_exception);
+  assert(event == OOP_WRITE);
+  assert(fd == session->out.fd);
+
+  channel_io_flush(&session->super, &session->out);
+  return OOP_CONTINUE;
+}
+
+static void *
+oop_write_stderr(oop_source *source UNUSED,
+		 int fd, oop_event event, void *state)
+{
+  CAST_SUBTYPE(client_session, session, (struct lsh_object *) state);
+
+  assert(event == OOP_WRITE);
+  assert(fd == session->err.fd);
+
+  channel_io_flush(&session->super, &session->err);
+  return OOP_CONTINUE;
 }
 
 
+/* FIXME: Use length, pointer instead of a string */
 /* Receive channel data */
 static void
-do_receive(struct ssh_channel *c,
+do_receive(struct ssh_channel *s,
 	   int type, struct lsh_string *data)
 {
-  CAST(client_session_channel, closure, c);
+  CAST(client_session, session, s);
   
   switch(type)
     {
     case CHANNEL_DATA:
-      A_WRITE(&closure->out->write_buffer->super, data);
+      channel_io_write(&session->super, &session->out,
+		       oop_write_stdout,
+		       STRING_LD(data));
       break;
     case CHANNEL_STDERR_DATA:
-      A_WRITE(&closure->err->write_buffer->super, data);
+      channel_io_write(&session->super, &session->err,
+		       oop_write_stderr,
+		       STRING_LD(data));
       break;
     default:
       fatal("Internal error!\n");
     }
+}
+
+/* Reading stdin */
+/* FIXME: Escape char handling */
+
+static void *
+oop_read_stdin(oop_source *source UNUSED,
+	       int fd, oop_event event, void *state)
+{
+  CAST(client_session, session, (struct lsh_object *) state);
+  uint32_t done;
+  
+  assert(fd == session->in.fd);
+  assert(event == OOP_READ);
+
+  done = channel_io_read(&session->super, &session->in);
+
+  if (done > 0)
+    {
+      /* FIXME: Look for escape char */
+      channel_transmit_data(&session->super,
+			    done, lsh_string_data(session->in.buffer));
+    }
+  else
+    {
+      /* FIXME: We ought to warn the user if we're out of window
+	 space, and turn the terminal back into canonical mode. */
+      ;
+    }
+  return OOP_CONTINUE;
 }
 
 /* We may send more data */
@@ -102,132 +187,73 @@ static void
 do_send_adjust(struct ssh_channel *s,
 	       uint32_t i UNUSED)
 {
-  CAST(client_session_channel, self, s);
+  CAST(client_session, session, s);
 
-  assert(self->in->read);
-
-  lsh_oop_register_read_fd(self->in);
+  channel_io_start_read(&session->super, &session->in, oop_read_stdin);
 }
 
-/* Escape char handling */
-
-static struct io_callback *
-client_read_stdin(struct client_session_channel *session)
+/* Called when a session channel which a remote shell */
+DEFINE_COMMAND(client_start_io)
+     (struct command *s UNUSED,
+      struct lsh_object *x,
+      struct command_continuation *c,
+      struct exception_handler *e UNUSED)
 {
-  struct abstract_write *write = make_channel_write(&session->super);
+  CAST(client_session, session, x);
 
-  if (session->escape)
-    write = make_handle_escape(session->escape, write);
+  session->super.receive = do_receive;
+  session->super.send_adjust = do_send_adjust;
+  session->super.eof = do_client_session_eof;
+
+  session->super.sources ++;
+
+  /* One reference each for stdout and stderr, and one more for the
+     exit-status/exit-signal message */
+  session->super.sinks += 3;
   
-  return make_read_data(&session->super, write);
-}
-
-/* We have a remote shell */
-static void
-do_client_io(struct command *s UNUSED,
-	     struct lsh_object *x,
-	     struct command_continuation *c,
-	     struct exception_handler *e UNUSED)
-
-{
-  CAST(client_session_channel, session, x);
-  struct ssh_channel *channel = &session->super;
-  assert(x);
-
-  /* Set up write fd:s. */
-  
-  channel->receive = do_receive;
-
-  /* FIXME: It seems a little kludgy to modify exception handlers
-   * here; it would be better to create the fd-objects at a point
-   * where the right exception handlers can be installed from the
-   * start. */
-  session->out->e
-    = make_channel_io_exception_handler(channel,
-					"stdout: ", 0,
-					session->out->e,
-					HANDLER_CONTEXT);
-
-  session->err->e
-    = make_channel_io_exception_handler(channel,
-					"stderr: ", 0,
-					session->err->e,
-					HANDLER_CONTEXT);
-
-  /* Set up the fd we read from. */
-  channel->send_adjust = do_send_adjust;
-
-  /* Setup escape char handler, if appropriate. */
-  session->in->read = client_read_stdin(session);
-
-  /* FIXME: Perhaps there is some way to arrange that channel.c calls
-   * the CHANNEL_SEND_ADJUST method instead? */
+  /* FIXME: Setup escape handler, and raw tty? */
   if (session->super.send_window_size)
-    lsh_oop_register_read_fd(session->in);
+    channel_io_start_read(&session->super, &session->in, oop_read_stdin);
 
-  /* FIXME: We should also arrange so that the tty is reset before we
-   * close it. */
-  session->in->close_callback
-    = make_channel_read_close_callback(channel);
-
-  /* Make sure stdio is closed properly if the channel or connection dies */
-  remember_resource(channel->resources, &session->in->super);
-  remember_resource(channel->resources, &session->out->super);
-  remember_resource(channel->resources, &session->err->super);
-  
-  ALIST_SET(channel->request_types, ATOM_EXIT_STATUS,
+  ALIST_SET(session->super.request_types, ATOM_EXIT_STATUS,
 	    &make_handle_exit_status(session->exit_status)->super);
-  ALIST_SET(channel->request_types, ATOM_EXIT_SIGNAL,
+  ALIST_SET(session->super.request_types, ATOM_EXIT_SIGNAL,
 	    &make_handle_exit_signal(session->exit_status)->super);
 
-  channel->eof = do_client_session_eof;
-      
-  COMMAND_RETURN(c, channel);
+  channel_start_receive(&session->super, lsh_string_length(session->out.state->buffer));
+
+  COMMAND_RETURN(c, session);
 }
 
-struct command client_io =
-{ STATIC_HEADER, do_client_io };
-
+#define CLIENT_READ_BUFFER_SIZE 0x4000
 
 struct ssh_channel *
-make_client_session_channel(struct lsh_fd *in,
-			    struct lsh_fd *out,
-			    struct lsh_fd *err,
+make_client_session_channel(int in, int out, int err,
 			    struct escape_info *escape,
 			    uint32_t initial_window,
 			    int *exit_status)
 {
-  NEW(client_session_channel, self);
+  NEW(client_session, self);
 
-  trace("make_client_session_channel\n");
-  init_channel(&self->super);
+  trace("make_client_session\n");
+  init_channel(&self->super, do_kill_client_session);
 
-  /* Makes sure the pending_close bit is set whenever this session
-   * dies, no matter when or how. */
-  self->super.close = do_client_session_close;
-
-  /* We could miss the server's exit-status or exit-signal message if
-   * we close the channel directly at EOF. So don't do that.
-   *
-   * FIXME: Perhaps we need to set this bit again in do_exit_status
-   * and do_exit_signal. */
-  self->super.flags &= ~CHANNEL_CLOSE_AT_EOF;
-  
-  /* FIXME: We make rec_window_size non-zero here, but we don't setup
-   * the receive pointer until later, in do_client_io. That's bad. Do
-   * something similar to server_session.c: Add an inital_window
-   * attribute, and call channel_start_receive from client_io. */
-  self->super.rec_window_size = initial_window;
+  /* Set to initial_window in client_start_io */
+  self->super.rec_window_size = 0;
 
   /* FIXME: Make maximum packet size configurable */
   self->super.rec_max_packet = SSH_MAX_PACKET;
 
   self->super.request_types = make_alist(0, -1);
 
-  /* self->expect_close = 0; */
-  self->in = in;
-  self->out = out;
-  self->err = err;
+  init_channel_read_state(&self->in, in, CLIENT_READ_BUFFER_SIZE);
+  init_channel_write_state(&self->out, out, initial_window);
+  init_channel_write_state(&self->err, err, initial_window);
+
+  io_register_fd(in, "session stdin");
+  io_register_fd(out, "session stdout");
+  io_register_fd(err, "session stderr");
+  
   self->escape = escape;
 
 #if 0
@@ -235,14 +261,7 @@ make_client_session_channel(struct lsh_fd *in,
   if (self->escape)
     self->escape->dispatch['b'] = make_send_break(self->super);
 #endif
-  remember_resource(self->super.resources, &in->super);
-  remember_resource(self->super.resources, &out->super);
-  remember_resource(self->super.resources, &err->super);
-  
-  /* Flow control */
-  out->write_buffer->report = &self->super.super;
-  err->write_buffer->report = &self->super.super;
-  
+
   self->exit_status = exit_status;
   
   return &self->super;
