a6d20544c809b909c89fa773cd13bf4b3bbcd951
Niels Möller <nisse@lysator.liu.se>
2005-09-20 17:40:53 +0200

    Don't include abstract_io.h, format.h, ssh.h and xalloc.h. Include atoms.h (class service_handler, do_service_request) (make_service_request_handler, class offer_service) (do_offer_service, make_offer_service): Deleted.
    Rev: src/server.c:1.79.2.2

--- src/server.c
+++ src/server.c
@@ -33,127 +33,13 @@
 
 #include "server.h"
 
-#include "abstract_io.h"
-#include "format.h"
+#include "atoms.h"
 #include "io.h"
 #include "lsh_string.h"
 #include "sexp.h"
 #include "spki.h"
-#include "ssh.h"
 #include "werror.h"
-#include "xalloc.h"
 
-#if 0
-#include "server.c.x"
-
-
-static struct lsh_string *
-format_service_accept(int name)
-{
-  return ssh_format("%c%a", SSH_MSG_SERVICE_ACCEPT, name);
-}
-
-
-/* GABA:
-   (class
-     (name service_handler)
-     (super packet_handler)
-     (vars
-       (services object alist)
-       (c object command_continuation)
-       (e object exception_handler)))
-*/
-
-static void
-do_service_request(struct packet_handler *c,
-		   struct ssh_connection *connection,
-		   struct lsh_string *packet)
-{
-  CAST(service_handler, closure, c);
-
-  struct simple_buffer buffer;
-  unsigned msg_number;
-  int name;
-  
-  simple_buffer_init(&buffer, STRING_LD(packet));
-
-  if (parse_uint8(&buffer, &msg_number)
-      && (msg_number == SSH_MSG_SERVICE_REQUEST)
-      && parse_atom(&buffer, &name)
-      && parse_eod(&buffer))
-    {
-      if (name)
-	{
-	  CAST_SUBTYPE(command, service, ALIST_GET(closure->services, name));
-	  if (service)
-	    {
-	      /* Don't accept any further service requests */
-	      connection->dispatch[SSH_MSG_SERVICE_REQUEST]
-		= &connection_fail_handler;
-
-	      /* Start service */
-	      connection_send(connection, format_service_accept(name));
-	      
-	      COMMAND_CALL(service, connection,
-			   closure->c, closure->e);
-	      return;
-	    }
-	}
-      EXCEPTION_RAISE(connection->e,
-		      make_protocol_exception(SSH_DISCONNECT_SERVICE_NOT_AVAILABLE, NULL));
-      
-    }
-  else
-    PROTOCOL_ERROR(connection->e, "Invalid SERVICE_REQUEST message");
-}
-
-static struct packet_handler *
-make_service_request_handler(struct alist *services,
-			     struct command_continuation *c,
-			     struct exception_handler *e)
-{
-  NEW(service_handler, self);
-
-  self->super.handler = do_service_request;
-  self->services = services;
-  self->c = c;
-  self->e = e;
-  
-  return &self->super;
-}
-
-     
-/* GABA:
-   (class
-     (name offer_service)
-     (super command)
-     (vars
-       (services object alist)))
-*/
-
-static void
-do_offer_service(struct command *s,
-		 struct lsh_object *x,
-		 struct command_continuation *c,
-		 struct exception_handler *e)
-{
-  CAST(offer_service, self, s);
-  CAST(ssh_connection, connection, x);
-
-  connection->dispatch[SSH_MSG_SERVICE_REQUEST]
-    = make_service_request_handler(self->services, c, e);
-}
-
-struct command *make_offer_service(struct alist *services)
-{
-  NEW(offer_service, self);
-
-  self->super.call = do_offer_service;
-  self->services = services;
-
-  return &self->super;
-}
-#endif
 
 /* Read server's private key */
 
