83f7a304904e48317a1a8577d48c173add983c11
Niels Möller <nisse@lysator.liu.se>
2005-09-21 00:46:28 +0200

    (transport_process_packet): USe trace, not debug, for received message type.
    Rev: src/transport.c:1.6.2.16

--- src/transport.c
+++ src/transport.c
@@ -268,7 +268,7 @@ transport_process_packet(struct transport_connection *connection,
   data = lsh_string_data(packet);
   msg = data[0];
 
-  debug("Received %T (%i) message, length %i\n", msg, msg, length);
+  trace("Received %T (%i) message, length %i\n", msg, msg, length);
   
   /* Messages of type IGNORE, DISCONNECT and DEBUG are always
      acceptable. */
