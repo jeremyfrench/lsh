0ae4368987ef546d051c06b76624dee8d458b26d
Niels Möller <nisse@lysator.liu.se>
2006-02-13 17:55:15 +0100

    (server_parse_config_file): New function. (init_server_config, make_server_config): New functions. (server_argp): New config file parser, responsible for options related to the configuration files, and for processing of the configuration file.
    Rev: src/server.c:1.79.2.3
    Rev: src/server.h:1.21.2.3

--- src/server.c
+++ src/server.c
@@ -34,12 +34,17 @@
 #include "server.h"
 
 #include "atoms.h"
+#include "environ.h"
+#include "format.h"
 #include "io.h"
 #include "lsh_string.h"
 #include "sexp.h"
 #include "spki.h"
-#include "werror.h"
+#include "xalloc.h"
 
+#define GABA_DEFINE
+#include "server.h.x"
+#undef GABA_DEFINE
 
 /* Read server's private key */
 
@@ -111,3 +116,188 @@ read_host_key(const char *file,
     }
   return 1;
 }
+
+
+/* The config file is located, in order of decreasing precendence:
+ *
+ *   1. The --config-file command line option.
+ *
+ *   2. The <PROGRAM>_CONFIG_FILE environment variable.
+ *
+ *   3. The LSHD_CONFIG_DIR environment variable, combined with the
+ *      default file name.
+ *
+ *   4. The default directory, and the default file name, e.g.,
+ *       /etc/lshd/lshd_config.
+ */ 
+static int
+server_parse_config_file(const char *file,
+			 const char *default_file,			
+			 const char *env_file,
+			 const struct config_parser *parser,
+			 void *input)
+{
+  struct lsh_string *s = NULL;
+  struct lsh_string *contents;
+  int res;
+
+  int fd;
+
+  if (!file)
+    file = getenv(env_file);
+
+  if (!file)
+    {
+      const char *dir;
+
+      GET_FILE_ENV(dir, LSHD_CONFIG_DIR);
+      s = ssh_format("%lz/%lz", dir, default_file);
+      file = lsh_get_cstring(s);
+    }
+
+  fd = open(file, O_RDONLY);
+  if (fd < 0)
+    {
+      werror("Opening config file `%z' failed: %e.\n",
+	     file, errno);
+      lsh_string_free(s);
+      return errno;
+    }
+
+  contents = io_read_file_raw(fd, 10000);
+  close(fd);
+  if (!contents)
+    {
+      werror("Reading config file `%z' failed: %e.\n",
+	     file, errno);
+
+      lsh_string_free(s);
+      return errno;
+    }
+
+  res = server_config_parse_string(parser, lsh_get_cstring(s),
+				   STRING_LD(contents),
+				   input);
+  lsh_string_free(s);
+  lsh_string_free(contents);  
+
+  return res;
+}
+
+void
+init_server_config(struct server_config *self,
+		   const struct config_parser *parser,
+		   const char *default_file,			
+		   const char *env_variable)
+{
+  init_werror_config(&self->super);
+
+  self->parser = parser;
+  self->default_file = default_file;
+  self->env_variable = env_variable;
+
+  self->config_file = NULL;
+  self->use_example = 0;
+}
+
+struct server_config *
+make_server_config(const struct config_parser *parser,
+		   const char *default_file,			
+		   const char *env_variable)
+{
+  NEW(server_config, self);
+  init_server_config(self, parser, default_file, env_variable);
+  return self;
+}
+
+enum {  
+  OPT_CONFIG_FILE = 0x200,
+  OPT_PRINT_EXAMPLE,
+  OPT_USE_EXAMPLE,
+};
+
+static const struct argp_option
+server_options[] =
+{
+  { NULL, 0, NULL, 0, "Config file use:", 0 },
+  { "config-file", OPT_CONFIG_FILE, "FILE", 0,
+    "Location of configuration file.", 0 },
+  { "print-example-config", OPT_PRINT_EXAMPLE, NULL, 0,
+    "Print an example configuration file.", 0 },
+  { "use-example-config", OPT_USE_EXAMPLE, NULL, 0,
+    "Don't read any config file; use the example configuration." ,0 },
+  { NULL, 0, NULL, 0, NULL, 0 }
+};
+
+static const struct argp_child
+server_argp_children[] =
+{
+  { &werror_argp, 0, "", 0 },
+  { NULL, 0, NULL, 0}
+};
+
+static error_t
+server_argp_parser(int key, char *arg, struct argp_state *state)
+{
+  CAST_SUBTYPE(server_config, self, state->input);
+
+  switch(key)
+    {
+    default:
+      return ARGP_ERR_UNKNOWN;
+    case ARGP_KEY_INIT:
+      state->child_inputs[0] = &self->super;
+      break;
+
+    case ARGP_KEY_END:
+      if (self->config_file && self->use_example)
+	argp_error(state, "The options --config-file "
+		   "and --use-example-config are mutually exclusive.");
+
+      if (self->use_example)
+	{
+	  int err = server_config_parse_example(self->parser, self);
+	  if (err)
+	    argp_failure(state, EXIT_FAILURE, err,
+			 "Processing of the example configuration failed");
+	}
+      else
+	{
+	  int err = server_parse_config_file(self->config_file,
+					     self->default_file,
+					     self->env_variable,
+					     self->parser,
+					     self);
+	  if (err)
+	    argp_failure(state, EXIT_FAILURE, err,
+			 "Processing of the configuration file failed.");
+	}
+      if (!werror_init(&self->super))
+	argp_failure(state, EXIT_FAILURE, errno, "Failed to open log file");
+
+      break;
+
+    case OPT_CONFIG_FILE:
+      self->config_file = arg;
+      break;
+
+    case OPT_PRINT_EXAMPLE:
+      server_config_print_example(self->parser, stdout);
+      exit(EXIT_SUCCESS);
+
+    case OPT_USE_EXAMPLE:
+      self->use_example = 1;
+      break;
+    }
+  return 0;
+}
+
+const struct argp
+server_argp =
+{
+  server_options,
+  server_argp_parser,
+  NULL, NULL,
+  server_argp_children,
+  NULL, NULL
+};
--- src/server.h
+++ src/server.h
@@ -24,12 +24,44 @@
 #ifndef LSH_SERVER_H_INCLUDED
 #define LSH_SERVER_H_INCLUDED
 
-/* FIXME: Get rid of this file? */
 #include "alist.h"
+#include "server_config.h"
+#include "werror.h"
+
+#define GABA_DECLARE
+#include "server.h.x"
+#undef GABA_DECLARE
 
 int
 read_host_key(const char *file,
               struct alist *signature_algorithms,
               struct alist *keys);
 
+/* GABA:
+   (class
+     (name server_config)
+     (super werror_config)
+     (vars
+       (parser . "const struct config_parser *")
+       (default_file . "const char *")
+       (env_variable . "const char *")
+       
+       (config_file . "const char *")
+       (use_example . int)))
+*/
+
+void
+init_server_config(struct server_config *self,
+		   const struct config_parser *parser,
+		   const char *default_file,			
+		   const char *env_variable);
+
+struct server_config *
+make_server_config(const struct config_parser *parser,
+		   const char *default_file,			
+		   const char *env_variable);
+
+extern const struct argp server_argp;
+
 #endif /* LSH_SERVER_H_INCLUDED */
+
