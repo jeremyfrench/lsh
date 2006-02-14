17e43e71759c80225d44bb317ba9a98950982466
Niels Möller <nisse@lysator.liu.se>
2006-02-14 13:45:27 +0100

    Adapted event handler functions to return void.
    Rev: src/transport_forward.h:1.1.2.3

--- src/transport_forward.h
+++ src/transport_forward.h
@@ -50,15 +50,15 @@ init_transport_forward(struct transport_forward *self,
 		       void (*kill)(struct resource *s),
 		       struct transport_context *ctx,
 		       int ssh_input, int ssh_output,
-		       int (*event)(struct transport_connection *,
-				    enum transport_event event));
+		       void (*event)(struct transport_connection *,
+				     enum transport_event event));
 
 struct transport_forward *
 make_transport_forward(void (*kill)(struct resource *s),
 		       struct transport_context *ctx,
 		       int ssh_input, int ssh_output,
-		       int (*event)(struct transport_connection *,
-				    enum transport_event event));
+		       void (*event)(struct transport_connection *,
+				     enum transport_event event));
 
 void
 transport_forward_kill(struct transport_forward *self);
