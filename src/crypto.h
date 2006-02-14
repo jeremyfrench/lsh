33ad937b0887ec9c4fb65c1b7bf3f2f71f402e44
Niels Möller <nisse@lysator.liu.se>
2006-02-14 13:11:22 +0100

    Added comment on dh_hash_update and allocation.
    Rev: src/crypto.h:1.34.2.3

--- src/crypto.h
+++ src/crypto.h
@@ -313,6 +313,9 @@ init_dh_state(struct dh_state *self,
 	      const struct dh_params *params,
 	      struct kexinit_state *kex);
 
+/* NOTE: For all calls to this function, free = 1, which means that we
+   do some unnecessary allocation and freeing. But since key exchange
+   is pretty expensive anyway, it shouldn't matter much. */
 void
 dh_hash_update(struct dh_state *self,
 	       struct lsh_string *s, int free);
