fff819a9782a65ea5583a1a082894a4726ab2fab
Niels Möller <nisse@lysator.liu.se>
2006-02-27 16:25:53 +0100

    * src/pty-helper.h: New file. * src/pty-helper.c: New file.
    Rev: src/pty-helper.c:1.1.2.1
    Rev: src/pty-helper.h:1.1.2.1

---
+++ src/pty-helper.h
@@ -0,0 +1,54 @@
+#ifndef LSH_PTY_HELPER_H_INCLUDED
+#define LSH_PTY_HELPER_H_INCLUDED
+
+/* For pid_t */
+#include <unistd.h>
+
+/* For struct ucreds */
+#include <sys/types.h>
+#include <sys/socket.h>
+
+enum pty_request_type
+{
+  /* Creates a pty object reference, without doing anything with it. */
+  PTY_REQUEST_CREATE,
+
+  /* Creates a new pty pair. */
+  PTY_REQUEST_MASTER,
+
+  /* Updates utmp and wtmp for a login. */
+  PTY_REQUEST_LOGIN,
+
+  /* Updates utmp and wtmp for logout. */
+  PTY_REQUEST_LOGOUT,
+
+  /* Deletes the object, with no further processing. */
+  PTY_REQUEST_DESTROY
+};
+
+struct pty_message
+{
+  /* Ordinary data */
+  struct {
+    int type;
+    int ref;
+    unsigned length;
+  } header;
+
+  char *data;
+  
+  /* Transferred credentials */
+  int has_creds;
+  struct ucred creds;
+
+  /* Transferred fd (-1 if none) */
+  int fd;
+};
+
+int
+pty_send_message(int socket, const struct pty_message *message);
+
+int
+pty_recv_message(int socket, struct pty_message *message);
+
+#endif /* LSH_PTY_HELPER_H_INCLUDED */
