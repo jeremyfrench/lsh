01548ee3777baa8ee886101a417532b9b365f11f
Niels Möller <nisse@lysator.liu.se>
2008-05-12 21:26:31 +0200

    (class client_session_action): New class. (class client_session): New attributes actions, action_next, action_done, replacing the attribute requests. (class client_options)): Deleted, merged with lsh_options in lsh.c.
    Rev: src/client.h:1.66

--- src/client.h
+++ src/client.h
@@ -30,11 +30,11 @@
 #include "channel_io.h"
 #include "werror.h"
 
+struct client_session;
+
 #define GABA_DECLARE
 #include "client.h.x"
 #undef GABA_DECLARE
-
-/* FIXME: Rename to client_connection? */
 
 /* GABA:
    (class
@@ -85,9 +85,6 @@ client_gateway_random_request(struct client_connection *connection,
 			      uint32_t length, const uint8_t *packet,
 			      struct gateway_connection *gateway);
 
-/* The argp option group for actions. */
-#define CLIENT_ARGP_ACTION_GROUP 100
-#define CLIENT_ARGP_MODIFIER_GROUP 200
 
 /* GABA:
    (class
@@ -113,23 +110,41 @@ static void do_##name(struct lsh_callback *self UNUSED)
 */
 
 struct escape_info *make_escape_info(uint8_t escape);
+struct escape_info *
+make_client_escape(uint8_t escape);
+
 struct abstract_write *
 make_handle_escape(struct escape_info *info, struct abstract_write *next);
 
-struct channel_request *make_handle_exit_status(int *exit_code);
-struct channel_request *make_handle_exit_signal(int *exit_code);
-
 struct command *make_open_session_command(struct ssh_channel *session);
 
+struct command *make_background_process(int write_pid);
 
-extern struct command request_shell;
-#define REQUEST_SHELL (&request_shell.super)
 
-struct command *
-make_session_channel_request(int type, struct lsh_string *arg);
+/* GABA:
+   (class
+     (name client_session_action)
+     (vars
+       ; If non-zero, wait for previous requests to complete.
+       (serial . int)
 
-extern struct command client_start_io;
-#define CLIENT_START_IO (&client_start_io.super)
+       (start method void "struct client_session *session")
+       (success method void "struct client_session *session")
+       ; Returns 1 if we should continue despite the error.
+       (failure method int "struct client_session *session")))
+*/
+
+extern struct client_session_action
+client_request_shell;
+
+struct client_session_action *
+make_exec_action(struct lsh_string *command);
+
+struct client_session_action *
+make_subsystem_action(struct lsh_string *subsystem);
+
+struct client_session_action *
+make_pty_action(struct interact *tty);
 
 /* Initiate and manage a session */
 /* GABA:
@@ -143,16 +158,20 @@ extern struct command client_start_io;
        (out struct channel_write_state)
        (err struct channel_write_state)
 
-       ; The pty-allocation code adds resources here.
        (resources object resource_list)
 
-       ; Commands to be invoked after the session is opened.
-       (requests struct object_queue)
+       ; Actions to be invoked after the session is opened.
+       (actions object object_list)
+       ; Next action to start
+       (action_next . unsigned)
+       ; The action that the next received success or failure
+       ; message corresponds to.
+       (action_done . unsigned)
 
        ; For errors when opening the channel, or when sending the
        ; channel requests. It's not clear what's the proper place for
        ; the error handling.
-       (e object exception_handler)
+       ; (e object exception_handler)
 
        ; Escape char handling
        (escape object escape_info)
@@ -162,19 +181,11 @@ extern struct command client_start_io;
 
 struct client_session *
 make_client_session_channel(int in, int out, int err,
+			    struct object_list *actions,
 			    struct exception_handler *e,
 			    struct escape_info *escape,
 			    uint32_t initial_window,
 			    int *exit_status);
-
-struct command *
-make_exec_request(struct lsh_string *command);
-
-struct command *
-make_pty_request(struct interact *tty);
-
-struct command *
-make_subsystem_request(struct lsh_string *subsystem);
 
 #if 0
 void
@@ -191,88 +202,8 @@ make_forward_x11(const char *display_string,
 struct client_x11_display *
 make_client_x11_display(const char *display, struct lsh_string *fake);
 
-/* FIXME: Move options parsing to lsh.c, there's no longer any lshg
-   program to share client-side options with. */
-
-/* GABA:
-   (class
-     (name client_options)
-     (super werror_config)
-     (vars
-       ;; Used only by lsh, NULL for lshg.
-       (random object randomness)
-
-       (tty object interact)
-
-       ; -1 means default.
-       (escape . int)
-       
-       (handler object exception_handler)
-
-       (exit_code . "int *")
-
-       (not . int)
-       (port . "const char *")
-       (target . "const char *")
-
-       (local_user . "char *")
-       (user . "char *")
-
-       (with_remote_peers . int)
-       
-       ; -1 means default behaviour
-       (with_pty . int)
-
-       (with_x11 . int)
-       
-       ; Session modifiers
-       (stdin_file . "const char *")
-       (stdout_file . "const char *")
-       (stderr_file . "const char *")
-
-       ; True if the process's stdin or pty (respectively) has been used. 
-       (used_stdin . int)
-       (used_pty . int)
-       (used_x11 . int)
-
-       ; Should -B write the pid to stdout?
-       (write_pid . int)
-       
-       ; True if the client should detach when a session closes (useful for gateways)
-       (detach_end . int)
-
-       ; Inhibit actions, used to not create actions from environment parsing.
-       (inhibit_actions . int)
-
-       (start_shell . int)
-       (remote_forward . int)
-       (actions struct object_queue)
-
-       ; Resources that are created during argument parsing. These should be adopted
-       ; by the connection once it is up and running.
-       (resources object resource_list)))
-*/
-
-void
-init_client_options(struct client_options *options,
-		    struct randomness *random,
-		    struct exception_handler *handler,
-		    int *exit_code);
-
-struct command *
-client_add_action(struct client_options *options,
-		  struct command *action);
-struct command *
-client_prepend_action(struct client_options *options,
-		      struct command *action);
-
-int
-client_parse_forward_arg(char *arg,
-			 unsigned long *listen_port,
-			 struct address_info **target);
-
-extern const struct argp client_argp;
-
+struct lsh_string *
+client_rebuild_command_line(unsigned argc, char **argv);
 
 void
 env_parse(const struct argp *argp,
