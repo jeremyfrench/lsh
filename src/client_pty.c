d90e04bbda2bac1dc584b6be76ad46542ddb85ae
Niels Möller <nisse@lysator.liu.se>
2008-05-14 15:31:17 +0200

    (do_kill_client_tty_resource): Check alive flag. New trace message.
    Rev: src/client_pty.c:1.30

--- src/client_pty.c
+++ src/client_pty.c
@@ -51,10 +51,15 @@ static void
 do_kill_client_tty_resource(struct resource *s)
 {
   CAST(client_tty_resource, self, s);
-  self->super.alive = 0;
-  INTERACT_SET_ATTRIBUTES(self->tty, self->attr);
-  /* Tell the werror functions that terminal mode is restored. */
-  set_error_raw(0);
+  trace("do_kill_client_tty_resource\n");
+
+  if (self->super.alive)
+    {
+      self->super.alive = 0;
+      INTERACT_SET_ATTRIBUTES(self->tty, self->attr);
+      /* Tell the werror functions that terminal mode is restored. */
+      set_error_raw(0);
+    }
 }
 
 static struct resource *
