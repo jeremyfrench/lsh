e6d571acee50207be67cdf0bf15fc9ac36f6a28f
Niels Möller <nisse@lysator.liu.se>
2008-05-19 21:10:46 +0200

    Forward declare struct winsize and struct termios. (class terminal_attributes): Deleted. (class window_change_callback): Change callback argument, it now gets the new window size. (class interact): Deleted class. Use ordinary functions instead.
    Rev: src/interact.h:1.16

--- src/interact.h
+++ src/interact.h
@@ -6,7 +6,7 @@
 
 /* lsh, an implementation of the ssh protocol
  *
- * Copyright (C) 1999 Niels Möller
+ * Copyright (C) 1999, 2008 Niels Möller
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -29,38 +29,22 @@
 #include "lsh.h"
 
 /* Forward declaration */
-struct interact;
-struct terminal_dimensions;
+struct winsize; /* In sys/ioctl.h */
+struct termios;
 
 #define GABA_DECLARE
 #include "interact.h.x"
 #undef GABA_DECLARE
 
-/* Abstract class defining methods needed to communicate with the
- * user's terminal. */
 
-/* FIXME: This abstraction is unnecessary. Some methods in the
-   interactive class for switching the terminal to raw mode and back
-   should be sufficient. */
-/* GABA:
-   (class
-     (name terminal_attributes)
-     (vars
-       (make_raw method (object terminal_attributes))
-       (encode method (string))))
-*/
-
-#define TERM_MAKE_RAW(t) ((t)->make_raw((t)))
-#define TERM_ENCODE(t) ((t)->encode((t)))
+/* Methods needed to communicate with the user's terminal. */
 
 /* GABA:
    (class
      (name window_change_callback)
      (vars
-       (f method void "struct interact *i")))
+       (f method void "const struct winsize *dims")))
 */
-
-#define WINDOW_CHANGE_CALLBACK(c, i) ((c)->f((c), (i)))
 
 /* GABA:
    (class
@@ -76,50 +60,34 @@ struct terminal_dimensions;
 struct interact_dialog *
 make_interact_dialog(unsigned nprompt);
 
-/* GABA:
-   (class
-     (name interact)
-     (vars
-       (is_tty method int)
-       ; Consumes the prompt
-       (read_password method (string)
-                  "uint32_t max_length"
-		  "const struct lsh_string *prompt")
-       (set_askpass method void "const char *askpass")
-       (yes_or_no method int
-                  "const struct lsh_string *prompt"
-		  "int def" "int free")
-       (dialog method int
-		  "const struct interact_dialog *dialog")
+int
+interact_is_tty(void);
 
-       (get_attributes method (object terminal_attributes) )
-       (set_attributes method int "struct terminal_attributes *attr")
-       
-       (window_size method int "struct terminal_dimensions *")
-       (window_change_subscribe method (object resource)
-		  "struct window_change_callback *callback")))
-*/
+void
+interact_set_askpass(const char *askpass);
 
-#define INTERACT_IS_TTY(i) \
-  ((i)->is_tty((i)))
-#define INTERACT_READ_PASSWORD(i, l, p) \
-  ((i)->read_password((i), (l), (p)))
-#define INTERACT_SET_ASKPASS(i, a) \
-  ((i)->set_askpass((i), (a)))
-#define INTERACT_YES_OR_NO(i, p, d, f) \
-  ((i)->yes_or_no((i), (p), (d), (f)))
-#define INTERACT_DIALOG(i, d) \
-  ((i)->dialog((i), (d)))
-#define INTERACT_GET_ATTRIBUTES(i) \
-  ((i)->get_attributes((i)))
-#define INTERACT_SET_ATTRIBUTES(i, t) \
-  ((i)->set_attributes((i), (t)))
-#define INTERACT_WINDOW_SIZE(i, d) \
-  ((i)->window_size((i), (d)))
-#define INTERACT_WINDOW_SUBSCRIBE(i, c) \
-  ((i)->window_change_subscribe((i), (c)))
+int
+interact_yes_or_no(const struct lsh_string *prompt, int def, int free);
 
-struct interact *
-make_unix_interact(void);
+struct lsh_string *
+interact_read_password(const struct lsh_string *prompt);
+
+int
+interact_dialog(const struct interact_dialog *dialog);
+
+int
+interact_set_mode(int raw);
+
+int
+interact_get_window_size(struct winsize *dims);
+
+struct resource *
+interact_on_window_change(struct window_change_callback *c);
+
+const struct termios *
+interact_get_terminal_mode(void);
+
+int
+unix_interact_init(int prepare_raw_mode);
 
 #endif /* LSH_INTERACT_H_INCLUDED */
